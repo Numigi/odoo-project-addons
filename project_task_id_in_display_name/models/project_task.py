6d794f8bd13db3021691d906f82cb947bf59de58
David Dufresne <dufresne.david.2@gmail.com>
2018-04-19 18:33:46 -0400

    Fix codacy issue

--- project_task_id_in_display_name/models/project_task.py
+++ project_task_id_in_display_name/models/project_task.py
@@ -2,7 +2,7 @@
 # © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import api, fields, models
+from odoo import api, models
 
 
 class ProjectTaskWithCode(models.Model):
