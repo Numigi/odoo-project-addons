89bd5ad2ef482956149ee5c6caf7b4e70ac6671e
David Dufresne <dufresne.david.2@gmail.com>
2018-05-03 16:12:37 -0400

    [11.0] Add module project_task_time_range

---
+++ project_task_time_range/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Project Task Time Range',
+    'version': '1.0.0',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'license': 'LGPL-3',
+    'category': 'Project',
+    'summary': 'Add fields Min and Max on project tasks.',
+    'depends': ['hr_timesheet'],
+    'data': [
+        'views/project_task.xml',
+    ],
+    'installable': True,
+}
