26c6c0adfff5ffe05c5675059e28c05fda866bef
David Dufresne <dufresne.david.2@gmail.com>
2018-05-22 18:33:39 -0400

    Codacy

--- project_task_deadline_from_project/tests/test_project_task.py
+++ project_task_deadline_from_project/tests/test_project_task.py
@@ -1,7 +1,6 @@
 # © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo.exceptions import ValidationError
 from odoo.tests import common
 
 
