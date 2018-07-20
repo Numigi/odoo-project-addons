c1bbd5167054c7604f761fc9d6b466e41d28cd68
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2018-07-20 11:04:58 -0400

    Remove unused modules (and unmigrated) and update website url in manifests (#13)
    * Remove unused modules (and unmigrated) and update website url in manifests
    
    * Remove unused modules

--- project_task_id_in_display_name/__manifest__.py
+++ project_task_id_in_display_name/__manifest__.py
@@ -7,6 +7,7 @@
     'version': '1.1.0',
     'author': 'Numigi',
     'maintainer': 'Numigi',
+    'website': 'https://bit.ly/numigi-com',
     'license': 'LGPL-3',
     'category': 'Project',
     'summary': 'Add the ID of a task to its displayed name.',
