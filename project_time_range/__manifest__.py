05e671eba88aac90c1e60ab1b3bcbbb3c955fc68
Jordi Riera <547282+foutoucour@users.noreply.github.com>
2018-11-26 16:38:05 -0500

    add project_time_range (TA#6153) (#18)
    * add project_time_range (TA#6153)
    * add case of planned_hours is None and update README

---
+++ project_time_range/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Project Time Range',
+    'version': '1.0.0',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'website': 'https://bit.ly/numigi-com',
+    'license': 'LGPL-3',
+    'category': 'Project',
+    'summary': 'Add fields Min and Max on project.',
+    'depends': ['project'],
+    'data': [
+        'views/project_project.xml',
+    ],
+    'installable': True,
+}
---
+++ project_time_range/models/project_project.py
@@ -0,0 +1,28 @@
+# © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models, api, _
+from odoo.exceptions import ValidationError
+
+
+class ProjectTaskWithMinAndMaxHours(models.Model):
+    """Add the fields min_hours and max_hours on tasks."""
+
+    _inherit = 'project.project'
+
+    min_hours = fields.Float('Minimum Planned Hours')
+    max_hours = fields.Float('Maximum Planned Hours')
+    planned_hours = fields.Float(string='Ideal Planned Hours')
+
+    @api.one
+    @api.constrains('planned_hours', 'min_hours', 'max_hours')
+    def _check_description(self):
+        if self.planned_hours > 0:
+            if self.min_hours > self.planned_hours:
+                raise ValidationError(
+                    _("Min Hours must be lesser than the planned hours.")
+                )
+            elif self.max_hours < self.planned_hours:
+                raise ValidationError(
+                    _("Max Hours must be greater than the planned hours.")
+                )
