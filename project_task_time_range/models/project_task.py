f5a124e30c3f28055e12018e0aad4c7b168a9e93
Jordi Riera <547282+foutoucour@users.noreply.github.com>
2018-11-26 16:41:58 -0500

    update project_task_time_range (TA#6155) (#19)
    * update project_task_time_range (TA#6155)
    * project_accurate_time_spent: set max hours in tests
    * planned_hours can be None (TA#6155)

--- project_task_time_range/models/project_task.py
+++ project_task_time_range/models/project_task.py
@@ -2,7 +2,8 @@
 # © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import fields, models
+from odoo import fields, models, api, _
+from odoo.exceptions import ValidationError
 
 
 class ProjectTaskWithMinAndMaxHours(models.Model):
@@ -20,3 +21,25 @@ class ProjectTaskWithPlannedHoursRenamed(models.Model):
     _inherit = 'project.task'
 
     planned_hours = fields.Float(string='Ideal Planned Hours')
+
+
+class ProjectTaskWithMinAndMaxConstraints(models.Model):
+    """ Add constraints on min_hours and max_hours
+
+    see TA#6155
+    """
+    _inherit = 'project.task'
+
+    @api.one
+    @api.constrains('planned_hours', 'min_hours', 'max_hours')
+    def _check_description(self):
+        # Case where planned_hours is different from 0 or None
+        if self.planned_hours:
+            if self.min_hours > self.planned_hours:
+                raise ValidationError(
+                    _("Min Hours must be lesser than the planned hours.")
+                )
+            elif self.max_hours < self.planned_hours:
+                raise ValidationError(
+                    _("Max Hours must be greater than the planned hours.")
+                )
