c0bc861ca0d8966b132ebe70f7d70a8f6b9cf46d
Jordi Riera <jordi.riera@numigi.com>
2019-01-17 21:12:20 -0500

    add project_task_subtask_same_project (TA#6753)

---
+++ project_task_subtask_same_project/tests/test_project_task.py
@@ -0,0 +1,47 @@
+# © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+
+from odoo.tests import common
+
+
+class TestProjectTaskSubTaskSameProject(common.SavepointCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.project_a = cls.env['project.project'].create({'name': 'projectA'})
+        cls.project_b = cls.env['project.project'].create({'name': 'projectB'})
+        cls.task_parent = cls.env['project.task'].create({
+            'name': 'Task Parent', 'project_id': cls.project_a.id
+        })
+        cls.subtask_1 = cls.env['project.task'].create({
+            'name': 'Task Child 1',
+            'project_id': cls.task_parent.project_id.id,
+            'parent_id': cls.task_parent.id,
+            'min_hours': 0.5,
+            'planned_hours': 1.0,
+            'max_hours': 2.0
+        })
+        cls.subtask_2 = cls.env['project.task'].create({
+            'name': 'Task Child 2',
+            'project_id': cls.task_parent.project_id.id,
+            'parent_id': cls.task_parent.id,
+            'min_hours': 0.5,
+            'planned_hours': 1.0,
+            'max_hours': 2.0
+        })
+
+    def test_whenParentTaskChangeProject_thenSubTaskInheritNewProject(self):
+        """
+        Given a parent task is on project A
+        And the subtasks are on project A too
+
+        When the parent task is changed to project B
+
+        Then the subtasks are changed to project B too.
+        """
+        self.task_parent.project_id = self.project_b.id
+
+        assert self.subtask_1.project_id == self.project_b
+        assert self.subtask_2.project_id == self.project_b
