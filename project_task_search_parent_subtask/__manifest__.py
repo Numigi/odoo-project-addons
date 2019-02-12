894e52ee033fa54969bafe2fb32b9e2d83513ac6
Jordi Riera <jordi.riera@numigi.com>
2019-02-12 16:35:17 -0500

    add project_task_search_parent_subtask (TA#9138)

---
+++ project_task_search_parent_subtask/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Project Task Filter Parent and Subtask',
+    'version': '12.0.1.0',
+    'category': 'Project',
+    'description': u"Add filter to find the parent tasks and the sub tasks.",
+    'author': 'Numigi',
+    'depends': [
+        'project',
+    ],
+    'data': [
+        'data/ir_ui_view.xml',
+    ],
+    'application': False,
+}
