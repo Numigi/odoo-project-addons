fb39f98bd5fb7e19cf0102620ba6c51c331ba052
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2019-03-06 14:52:58 -0500

    Project wip: no analytic entries (#36)
    * Add module project_wip

---
+++ project_wip/__manifest__.py
@@ -0,0 +1,22 @@
+# © 2019 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Project Work In Progress',
+    'version': '1.0.0',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'website': 'https://bit.ly/numigi-com',
+    'license': 'LGPL-3',
+    'category': 'Project',
+    'summary': 'Manage Work In Progress Accounting',
+    'depends': [
+        'account',
+        'project',
+        'project_type',
+    ],
+    'data': [
+        'views/account_move_no_analytic.xml',
+    ],
+    'installable': True,
+}
