5b23680dba7dfbecfe2a2f6e7d874ed3ddf5d09a
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2019-04-03 10:47:04 -0400

    Codacy fix

--- project_wip_outsourcing/tests/test_outsourcing_purchase_order.py
+++ project_wip_outsourcing/tests/test_outsourcing_purchase_order.py
@@ -2,8 +2,6 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 import pytest
-from datetime import datetime
-from odoo import fields
 from odoo.exceptions import ValidationError
 from .common import OutsourcingCase
 
