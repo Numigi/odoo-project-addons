83c1bf66af9e7e0d180f5b852e4b57f204298538
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2019-04-04 13:39:36 -0400

    Fix field outsourcing_line_ids in project_wip_outsourcing (#51)
    * Fix field outsourcing_line_ids in project_wip_outsourcing

---
+++ project_wip_outsourcing/tests/test_task_outsourcing_tab.py
@@ -0,0 +1,17 @@
+# © 2019 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from .common import OutsourcingCase
+
+
+class TestTaskOutsourcingTab(OutsourcingCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.order_2 = cls.order.copy()
+
+    def test_outsourcing_line_ids_contains_lines_from_all_po(self):
+        assert self.task.outsourcing_line_ids == (
+            self.order.order_line | self.order_2.order_line
+        )
