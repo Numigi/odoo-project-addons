c971d93bae31b253909bf0ad8b561f4004655ab0
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2019-04-04 12:58:45 -0400

    Codacy fix

--- project_wip_outsourcing/tests/test_task_outsourcing_tab.py
+++ project_wip_outsourcing/tests/test_task_outsourcing_tab.py
@@ -1,8 +1,6 @@
 # © 2019 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-import pytest
-from odoo.exceptions import ValidationError
 from .common import OutsourcingCase
 
 
