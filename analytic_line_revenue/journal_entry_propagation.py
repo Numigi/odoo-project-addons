6b79e1ad15eca7b90f5f7500ab519c7273dfbf7d
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2019-06-11 09:02:15 -0400

    Add module analytic_line_revenue (#59)
    * Add module analytic_line_revenue

---
+++ analytic_line_revenue/journal_entry_propagation.py
@@ -0,0 +1,19 @@
+# © 2019 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import models
+
+
+class AccountMoveLine(models.Model):
+
+    _inherit = 'account.move.line'
+
+    def create_analytic_lines(self):
+        result = super().create_analytic_lines()
+
+        revenue_type = self.env.ref('account.data_account_type_revenue')
+        revenue_lines = self.filtered(
+            lambda l: l.account_id.user_type_id == revenue_type)
+        revenue_lines.mapped('analytic_line_ids').write({'revenue': True})
+
+        return result
