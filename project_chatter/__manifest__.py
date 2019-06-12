bf7fe73ab7ccd2f75a1e1a9dbdb44060a1f6a1c8
Jordi Riera <547282+foutoucour@users.noreply.github.com>
2019-06-12 09:27:00 -0400

    add project_chatter (TA#12028) (#60)

---
+++ project_chatter/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2019 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Project Chatter',
+    'version': '1.0.0',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'website': 'https://bit.ly/numigi-com',
+    'license': 'LGPL-3',
+    'category': 'Project',
+    'summary': 'Add the chatter and the tracking of some fields for projects.',
+    'depends': ['project'],
+    'data': [
+        'views/project_project_views.xml',
+    ],
+    'installable': True,
+}
