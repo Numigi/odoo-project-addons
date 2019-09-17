b8a25480da2fd5634453294ba1f687bbef7c098c
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2019-09-17 16:02:29 -0400

    Fix main

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -12,7 +12,7 @@
     'summary': 'Install all addons required for testing.',
     'depends': [
         'stock_account',  # required for testing project_wip
-        'purchase_stock',  # required for testing project_wip_material
+        'purchase',  # required for testing project_wip_material
 
         'analytic_line_employee',
         'analytic_line_revenue',
