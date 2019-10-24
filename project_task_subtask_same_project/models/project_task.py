233db9085fc26a0ffc1a4280ca161f0af18cbd23
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2019-10-24 16:50:05 -0400

    Improve project_task_subtask_same_project
    Add constraint on subtasks.
    Hide field Sub-task project on projects.
    Remove irrelevant filter on sub-task kanban view.

--- project_task_subtask_same_project/models/project_task.py
+++ project_task_subtask_same_project/models/project_task.py
@@ -1,7 +1,8 @@
 # © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import models, api
+from odoo import models, api, _
+from odoo.exceptions import ValidationError
 
 
 class ProjectTaskSubtaskSameProject(models.Model):
@@ -11,7 +12,39 @@ class ProjectTaskSubtaskSameProject(models.Model):
     @api.multi
     def write(self, vals):
         """ Propagate the value of the project to the subtask when it is changed on the parent task."""
+        res = super().write(vals)
         for task in self:
             if task.child_ids and 'project_id' in vals:
                 task.child_ids.write({'project_id': vals['project_id']})
-        return super().write(vals)
+        return res
+
+    def action_subtask(self):
+        """Remove project filters from the subtask action context."""
+        res = super().action_subtask()
+
+        context_vars_to_remove = (
+            'search_default_project_id',
+            'search_default_parent_project_ids',  # defined by the module project_iteration
+        )
+
+        res['context'] = {
+            k: v for k, v in res['context'].items() if k not in context_vars_to_remove
+        }
+
+        return res
+
+    @api.constrains('project_id')
+    def _check_subtask_not_in_different_project(self):
+        subtasks = self.filtered(lambda t: t.parent_id)
+
+        for subtask in subtasks:
+            task = subtask.parent_id
+            if subtask.project_id != task.project_id:
+                raise ValidationError(_(
+                    'The task {task} is in the project {task_project}. '
+                    'The subtask {subtask} must be in the same project.'
+                ).format(
+                    task=task.display_name,
+                    task_project=task.project_id.display_name,
+                    subtask=subtask.display_name,
+                ))
