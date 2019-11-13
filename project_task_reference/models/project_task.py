65df1bf4a6d569188444bb66cacb4db99d29a074
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2019-11-13 10:05:32 -0500

    Codacy

--- project_task_reference/models/project_task.py
+++ project_task_reference/models/project_task.py
@@ -2,7 +2,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 import re
-from odoo import api, models, _
+from odoo import models, _
 from odoo.exceptions import MissingError
 from ..reference import TaskReference
 
