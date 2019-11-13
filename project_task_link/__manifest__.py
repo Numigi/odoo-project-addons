d17503300a7e7760ffe159be501e2b79244fbe40
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2019-11-13 14:56:22 -0500

    Add module project_task_link (#95)
    * Add module project_task_link
    
    * Add module project_task_link
    
    * Codacy
    
    * Add module project_task_reference
    
    Separate the logic of searching task based on strings.
    This module could be reused later to match a pull request title with a task.
    
    * Update README
    
    * Remove assert
    
    * Codacy

---
+++ project_task_link/__manifest__.py
@@ -0,0 +1,23 @@
+# © 2019 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Project Task Link',
+    'version': '1.1.0',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'website': 'https://bit.ly/numigi-com',
+    'license': 'LGPL-3',
+    'category': 'Project',
+    'summary': 'Dynamically add links to tasks in the web interface.',
+    'depends': [
+        'project_task_reference',
+
+        # required because of the HTML editor field.
+        'web_editor',
+    ],
+    'data': [
+        'views/assets.xml',
+    ],
+    'installable': True,
+}
