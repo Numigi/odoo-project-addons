e3b58390d93f1fb53c297f2c8238693ca4a192e6
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-01-13 09:48:10 -0500

    Add module project_material_direct (#104)
    * Add module project_material_direct

---
+++ project_material_direct/tests/test_stock_picking_type.py
@@ -0,0 +1,25 @@
+# © 2020 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+import pytest
+from odoo.exceptions import ValidationError
+from odoo.tests.common import SavepointCase
+
+
+class TestPickingType(SavepointCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.picking_type = cls.env['stock.picking.type'].create({
+            'name': 'Direct Consumption',
+            'code': 'consumption',
+            'is_direct_consumption': True,
+            'sequence_id': cls.env['ir.sequence'].create({
+                'name': 'Direct Consumption',
+            }).id,
+        })
+
+    def test_if_not_consumption__can_not_be_direct_consumption(self):
+        with pytest.raises(ValidationError):
+            self.picking_type.code = 'incoming'
