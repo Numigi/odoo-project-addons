a0b8d7e60ec47632fa580dce1d135eb72537f587
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-05-14 16:12:12 -0400

    Add configurable categories on project cost report (#129)

--- project_cost_report/models/account_analytic_line.py
+++ project_cost_report/models/account_analytic_line.py
@@ -7,14 +7,13 @@ from odoo import api, fields, models
 class AnalyticLine(models.Model):
     """Add extra computed fields to display in the report."""
 
-    _inherit = 'account.analytic.line'
+    _inherit = "account.analytic.line"
 
     unit_cost = fields.Monetary(
-        compute='_compute_unit_cost',
-        currency_field='company_currency_id',
+        compute="_compute_unit_cost", currency_field="company_currency_id"
     )
 
-    @api.depends('amount', 'unit_amount')
+    @api.depends("amount", "unit_amount")
     def _compute_unit_cost(self):
         for line in self:
             line.unit_cost = -(
