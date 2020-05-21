117abcc7af75cfc2b858bf4fe97b2ab8628d6b12
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-05-21 09:08:27 -0400

    Add a specific group for transfering WIP entries (#132)

--- project_wip/__manifest__.py
+++ project_wip/__manifest__.py
@@ -2,24 +2,21 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    'name': 'Project Work In Progress',
-    'version': '1.0.0',
-    'author': 'Numigi',
-    'maintainer': 'Numigi',
-    'website': 'https://bit.ly/numigi-com',
-    'license': 'LGPL-3',
-    'category': 'Project',
-    'summary': 'Manage Work In Progress Accounting',
-    'depends': [
-        'account',
-        'project',
-        'project_type',
+    "name": "Project Work In Progress",
+    "version": "1.1.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "LGPL-3",
+    "category": "Project",
+    "summary": "Manage Work In Progress Accounting",
+    "depends": ["account", "project", "project_type"],
+    "data": [
+        "security/res_groups.xml",
+        "security/ir.model.access.csv",
+        "views/account_move_no_analytic.xml",
+        "views/project_type.xml",
+        "views/project_wip_to_cgs.xml",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/account_move_no_analytic.xml',
-        'views/project_type.xml',
-        'views/project_wip_to_cgs.xml',
-    ],
-    'installable': True,
+    "installable": True,
 }
