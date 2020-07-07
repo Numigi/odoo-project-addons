4189b33be7e7f9054d5cc559714261312542186a
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-07-07 16:25:45 -0400

    TA#25016 Prevent wip entries with zero debit/credit

--- project_wip_supply_cost/tests/test_wip_journal_entries.py
+++ project_wip_supply_cost/tests/test_wip_journal_entries.py
@@ -9,124 +9,144 @@ from odoo.exceptions import ValidationError
 
 
 class WIPJournalEntriesCase(common.SavepointCase):
-
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.company = cls.env['res.company'].create({
-            'name': 'Test Company',
-        })
-        cls.manager = cls.env['res.users'].create({
-            'name': 'Manager',
-            'login': 'manager',
-            'email': 'manager@test.com',
-            'groups_id': [
-                (4, cls.env.ref('project.group_project_manager').id),
-            ],
-            'company_id': cls.company.id,
-            'company_ids': [(4, cls.company.id)],
-        })
+        cls.company = cls.env["res.company"].create({"name": "Test Company"})
+        cls.manager = cls.env["res.users"].create(
+            {
+                "name": "Manager",
+                "login": "manager",
+                "email": "manager@test.com",
+                "groups_id": [(4, cls.env.ref("project.group_project_manager").id)],
+                "company_id": cls.company.id,
+                "company_ids": [(4, cls.company.id)],
+            }
+        )
 
-        cls.timesheet_user = cls.env['res.users'].create({
-            'name': 'Timesheet User',
-            'login': 'timesheet_user@example.com',
-            'email': 'timesheet_user@example.com',
-            'groups_id': [
-                (4, cls.env.ref('hr_timesheet.group_hr_timesheet_user').id),
-            ],
-            'company_id': cls.company.id,
-            'company_ids': [(4, cls.company.id)],
-        })
+        cls.timesheet_user = cls.env["res.users"].create(
+            {
+                "name": "Timesheet User",
+                "login": "timesheet_user@example.com",
+                "email": "timesheet_user@example.com",
+                "groups_id": [
+                    (4, cls.env.ref("hr_timesheet.group_hr_timesheet_user").id)
+                ],
+                "company_id": cls.company.id,
+                "company_ids": [(4, cls.company.id)],
+            }
+        )
 
-        cls.employee = cls.env['hr.employee'].create({
-            'name': 'Timesheet User',
-            'user_id': cls.timesheet_user.id,
-        })
+        cls.employee = cls.env["hr.employee"].create(
+            {"name": "Timesheet User", "user_id": cls.timesheet_user.id}
+        )
 
-        cls.shop_supply_journal = cls.env['account.journal'].create({
-            'name': 'Shop Supply To WIP',
-            'code': 'SHOP',
-            'type': 'general',
-            'company_id': cls.company.id,
-        })
+        cls.shop_supply_journal = cls.env["account.journal"].create(
+            {
+                "name": "Shop Supply To WIP",
+                "code": "SHOP",
+                "type": "general",
+                "company_id": cls.company.id,
+            }
+        )
 
-        cls.cgs_journal = cls.env['account.journal'].create({
-            'name': 'Work in Progress',
-            'code': 'WIP',
-            'update_posted': True,
-            'type': 'general',
-            'company_id': cls.company.id,
-        })
+        cls.cgs_journal = cls.env["account.journal"].create(
+            {
+                "name": "Work in Progress",
+                "code": "WIP",
+                "update_posted": True,
+                "type": "general",
+                "company_id": cls.company.id,
+            }
+        )
 
-        cls.wip_account = cls.env['account.account'].create({
-            'name': 'Work In Progress',
-            'code': '140101',
-            'user_type_id': cls.env.ref('account.data_account_type_non_current_assets').id,
-            'reconcile': True,
-            'company_id': cls.company.id,
-        })
+        cls.wip_account = cls.env["account.account"].create(
+            {
+                "name": "Work In Progress",
+                "code": "140101",
+                "user_type_id": cls.env.ref(
+                    "account.data_account_type_non_current_assets"
+                ).id,
+                "reconcile": True,
+                "company_id": cls.company.id,
+            }
+        )
 
-        cls.cgs_account = cls.env['account.account'].create({
-            'name': 'Cost of Goods Sold',
-            'code': '510101',
-            'user_type_id': cls.env.ref('account.data_account_type_expenses').id,
-            'company_id': cls.company.id,
-        })
+        cls.cgs_account = cls.env["account.account"].create(
+            {
+                "name": "Cost of Goods Sold",
+                "code": "510101",
+                "user_type_id": cls.env.ref("account.data_account_type_expenses").id,
+                "company_id": cls.company.id,
+            }
+        )
 
-        cls.shop_supply_account = cls.env['account.account'].create({
-            'name': 'Shop Supply',
-            'code': '510201',
-            'user_type_id': cls.env.ref('account.data_account_type_expenses').id,
-            'company_id': cls.company.id,
-        })
+        cls.shop_supply_account = cls.env["account.account"].create(
+            {
+                "name": "Shop Supply",
+                "code": "510201",
+                "user_type_id": cls.env.ref("account.data_account_type_expenses").id,
+                "company_id": cls.company.id,
+            }
+        )
 
-        cls.env = cls.env(user=cls.manager, context={'force_company': cls.company.id})
+        cls.env = cls.env(user=cls.manager, context={"force_company": cls.company.id})
 
-        cls.env['project.project'].create({
-            'name': 'Job 123',
-            'company_id': cls.company.id,
-        })
+        cls.env["project.project"].create(
+            {"name": "Job 123", "company_id": cls.company.id}
+        )
 
         cls.shop_supply_rate = 15
-        cls.project_type = cls.env['project.type'].create({
-            'name': 'Trailer Refurb',
-            'wip_account_id': cls.wip_account.id,
-            'shop_supply_journal_id': cls.shop_supply_journal.id,
-            'shop_supply_account_id': cls.shop_supply_account.id,
-            'shop_supply_rate': cls.shop_supply_rate,
-            'cgs_account_id': cls.cgs_account.id,
-            'cgs_journal_id': cls.cgs_journal.id,
-        })
+        cls.project_type = cls.env["project.type"].create(
+            {
+                "name": "Trailer Refurb",
+                "wip_account_id": cls.wip_account.id,
+                "shop_supply_journal_id": cls.shop_supply_journal.id,
+                "shop_supply_account_id": cls.shop_supply_account.id,
+                "shop_supply_rate": cls.shop_supply_rate,
+                "cgs_account_id": cls.cgs_account.id,
+                "cgs_journal_id": cls.cgs_journal.id,
+            }
+        )
 
-        cls.project = cls.env['project.project'].create({
-            'name': 'Job 123',
-            'project_type_id': cls.project_type.id,
-            'company_id': cls.company.id,
-        })
+        cls.project = cls.env["project.project"].create(
+            {
+                "name": "Job 123",
+                "project_type_id": cls.project_type.id,
+                "company_id": cls.company.id,
+            }
+        )
 
-        cls.task = cls.env['project.task'].create({
-            'name': 'Task 450',
-            'project_id': cls.project.id,
-            'company_id': cls.company.id,
-        })
+        cls.task = cls.env["project.task"].create(
+            {
+                "name": "Task 450",
+                "project_id": cls.project.id,
+                "company_id": cls.company.id,
+            }
+        )
 
     @classmethod
     def _create_timesheet(cls, description="/", quantity=1, amount=50, date_=None):
         cls.employee.timesheet_cost = amount
-        line = cls.env['account.analytic.line'].sudo(cls.timesheet_user).create({
-            'company_id': cls.company.id,
-            'project_id': cls.project.id,
-            'task_id': cls.task.id,
-            'employee_id': cls.employee.id,
-            'name': description,
-            'date': date_ or datetime.now().date(),
-            'unit_amount': quantity,
-        })
+        line = (
+            cls.env["account.analytic.line"]
+            .sudo(cls.timesheet_user)
+            .create(
+                {
+                    "company_id": cls.company.id,
+                    "project_id": cls.project.id,
+                    "task_id": cls.task.id,
+                    "employee_id": cls.employee.id,
+                    "name": description,
+                    "date": date_ or datetime.now().date(),
+                    "unit_amount": quantity,
+                }
+            )
+        )
         return line.sudo()
 
 
 class TestWIPJournalEntries(WIPJournalEntriesCase):
-
     def test_if_shop_supply_account_filled__shop_supply_journal_must_be_filled(self):
         with pytest.raises(ValidationError):
             self.project_type.shop_supply_journal_id = False
@@ -141,31 +161,35 @@ class TestWIPJournalEntries(WIPJournalEntriesCase):
 
     def test_on_create_timesheet__account_move_is_posted(self):
         timesheet_line = self._create_timesheet()
-        assert timesheet_line.shop_supply_account_move_id.state == 'posted'
+        assert timesheet_line.shop_supply_account_move_id.state == "posted"
 
     def test_after_timesheet_write__account_move_is_posted(self):
         timesheet_line = self._create_timesheet()
         timesheet_line.unit_amount = -100
-        assert timesheet_line.shop_supply_account_move_id.state == 'posted'
+        assert timesheet_line.shop_supply_account_move_id.state == "posted"
 
     def test_account_move_has_analytic_lines(self):
         timesheet_line = self._create_timesheet()
         shop_supply_move = timesheet_line.shop_supply_account_move_id
-        assert shop_supply_move.mapped('line_ids.analytic_line_ids')
+        assert shop_supply_move.mapped("line_ids.analytic_line_ids")
 
     def test_analytic_line_is_shop_supply(self):
         timesheet_line = self._create_timesheet()
         shop_supply_move = timesheet_line.shop_supply_account_move_id
-        shop_supply_analytic_line = shop_supply_move.mapped('line_ids.analytic_line_ids')
+        shop_supply_analytic_line = shop_supply_move.mapped(
+            "line_ids.analytic_line_ids"
+        )
         assert shop_supply_analytic_line.is_shop_supply
 
     def _get_wip_move_line(self, timesheet_line):
         return timesheet_line.shop_supply_account_move_id.line_ids.filtered(
-            lambda l: l.account_id == self.wip_account)
+            lambda l: l.account_id == self.wip_account
+        )
 
     def _get_shop_supply_move_line(self, timesheet_line):
         return timesheet_line.shop_supply_account_move_id.line_ids.filtered(
-            lambda l: l.account_id == self.shop_supply_account)
+            lambda l: l.account_id == self.shop_supply_account
+        )
 
     def test_wip_move_line_analytic_account_is_project(self):
         timesheet_line = self._create_timesheet()
@@ -240,14 +264,13 @@ class TestWIPJournalEntries(WIPJournalEntriesCase):
 
     def test_if_new_project_requires_no_timesheet__account_move_reversed(self):
         timesheet_line = self._create_timesheet()
-        new_project = self.project.copy({'project_type_id': False})
-        new_task = self.task.copy({'project_id': new_project.id})
+        new_project = self.project.copy({"project_type_id": False})
+        new_task = self.task.copy({"project_id": new_project.id})
 
         wip_line = self._get_wip_move_line(timesheet_line)
-        timesheet_line.sudo(self.timesheet_user).write({
-            'project_id': new_project.id,
-            'task_id': new_task.id,
-        })
+        timesheet_line.sudo(self.timesheet_user).write(
+            {"project_id": new_project.id, "task_id": new_task.id}
+        )
         assert wip_line.reconciled
 
     def test_timesheet_unit_amount_can_be_changed_twice(self):
@@ -275,12 +298,15 @@ class TestWIPJournalEntries(WIPJournalEntriesCase):
     def test_after_change_project_on_timesheet__move_ref_contains_project_name(self):
         timesheet_line = self._create_timesheet()
         new_project = self.project.copy()
-        new_task = self.task.copy({'project_id': new_project.id})
-        timesheet_line.sudo(self.timesheet_user).write({
-            'project_id': new_project.id,
-            'task_id': new_task.id,
-        })
+        new_task = self.task.copy({"project_id": new_project.id})
+        timesheet_line.sudo(self.timesheet_user).write(
+            {"project_id": new_project.id, "task_id": new_task.id}
+        )
         assert new_project.name in timesheet_line.shop_supply_account_move_id.ref
+
+    def test_if_zero_hour__no_entry_created(self):
+        timesheet_line = self._create_timesheet(quantity=0)
+        assert not timesheet_line.shop_supply_account_move_id
 
 
 class TestTimesheetEntryTransferedToWip(WIPJournalEntriesCase):
@@ -297,13 +323,12 @@ class TestTimesheetEntryTransferedToWip(WIPJournalEntriesCase):
             self.timesheet_line.sudo(self.timesheet_user).unit_amount = 10
 
     def test_project_with_no_type_can_not_be_set(self):
-        new_project = self.project.copy({'project_type_id': False})
-        new_task = self.task.copy({'project_id': new_project.id})
+        new_project = self.project.copy({"project_type_id": False})
+        new_task = self.task.copy({"project_id": new_project.id})
         with pytest.raises(ValidationError):
-            self.timesheet_line.sudo(self.timesheet_user).write({
-                'project_id': new_project.id,
-                'task_id': new_task.id,
-            })
+            self.timesheet_line.sudo(self.timesheet_user).write(
+                {"project_id": new_project.id, "task_id": new_task.id}
+            )
 
     def test_timesheet_can_not_be_deleted(self):
         with pytest.raises(ValidationError):
--- project_wip_timesheet/wip_journal_entries.py
+++ project_wip_timesheet/wip_journal_entries.py
@@ -8,39 +8,43 @@ from odoo.exceptions import ValidationError
 class ProjectType(models.Model):
     """Add the salary account to project types."""
 
-    _inherit = 'project.type'
+    _inherit = "project.type"
 
     salary_journal_id = fields.Many2one(
-        'account.journal',
-        'Salary Journal',
+        "account.journal",
+        "Salary Journal",
         company_dependent=True,
-        help='Journal used for transfering salaries into work in progress '
-        'when creating or updating a timesheet entry.',
+        help="Journal used for transfering salaries into work in progress "
+        "when creating or updating a timesheet entry.",
     )
 
     salary_account_id = fields.Many2one(
-        'account.account',
-        'Salary Account',
+        "account.account",
+        "Salary Account",
         company_dependent=True,
-        help='Account used for the salaries (usually the credit part) '
-        'when transfering salaries into work in progress.',
+        help="Account used for the salaries (usually the credit part) "
+        "when transfering salaries into work in progress.",
     )
 
-    @api.constrains('salary_account_id', 'salary_journal_id', 'wip_account_id')
+    @api.constrains("salary_account_id", "salary_journal_id", "wip_account_id")
     def _check_required_fields_for_salary_entries(self):
         project_types_with_salary_account = self.filtered(lambda t: t.salary_account_id)
         for project_type in project_types_with_salary_account:
             if not project_type.wip_account_id:
-                raise ValidationError(_(
-                    'If the salary account is filled for a project type, '
-                    'the work in progress account must be filled as well.'
-                ))
+                raise ValidationError(
+                    _(
+                        "If the salary account is filled for a project type, "
+                        "the work in progress account must be filled as well."
+                    )
+                )
 
             if not project_type.salary_journal_id:
-                raise ValidationError(_(
-                    'If the salary account is filled for a project type, '
-                    'the salary journal must be filled as well.'
-                ))
+                raise ValidationError(
+                    _(
+                        "If the salary account is filled for a project type, "
+                        "the salary journal must be filled as well."
+                    )
+                )
 
 
 def _format_timesheet_line_description(timesheet_line):
@@ -50,11 +54,10 @@ def _format_timesheet_line_description(timesheet_line):
 
 class TimesheetLine(models.Model):
 
-    _inherit = 'account.analytic.line'
+    _inherit = "account.analytic.line"
 
     salary_account_move_id = fields.Many2one(
-        'account.move', 'Salary Journal Entry',
-        ondelete='restrict',
+        "account.move", "Salary Journal Entry", ondelete="restrict"
     )
 
     def _get_salary_journal(self):
@@ -89,7 +92,7 @@ class TimesheetLine(models.Model):
 
         :rtype: bool
         """
-        return bool(self._get_salary_account())
+        return self.amount and bool(self._get_salary_account())
 
     def _get_wip_move_line_vals(self):
         """Get the values for the wip account move line (usually the debit).
@@ -97,13 +100,13 @@ class TimesheetLine(models.Model):
         :rtype: dict
         """
         return {
-            'account_id': self._get_wip_account().id,
-            'name': self.name,
-            'debit': -self.amount if self.amount < 0 else 0,
-            'credit': self.amount if self.amount > 0 else 0,
-            'quantity': self.unit_amount,
-            'analytic_account_id': self.project_id.analytic_account_id.id,
-            'task_id': self.task_id.id,
+            "account_id": self._get_wip_account().id,
+            "name": self.name,
+            "debit": -self.amount if self.amount < 0 else 0,
+            "credit": self.amount if self.amount > 0 else 0,
+            "quantity": self.unit_amount,
+            "analytic_account_id": self.project_id.analytic_account_id.id,
+            "task_id": self.task_id.id,
         }
 
     def _get_salary_move_line_vals(self):
@@ -112,11 +115,11 @@ class TimesheetLine(models.Model):
         :rtype: dict
         """
         return {
-            'account_id': self._get_salary_account().id,
-            'name': self.name,
-            'debit': self.amount if self.amount > 0 else 0,
-            'credit': -self.amount if self.amount < 0 else 0,
-            'quantity': self.unit_amount,
+            "account_id": self._get_salary_account().id,
+            "name": self.name,
+            "debit": self.amount if self.amount > 0 else 0,
+            "credit": -self.amount if self.amount < 0 else 0,
+            "quantity": self.unit_amount,
         }
 
     def _get_wip_account_move_vals(self):
@@ -125,16 +128,15 @@ class TimesheetLine(models.Model):
         :rtype: dict
         """
         reference = "{project} / TA#{task}".format(
-            project=self.project_id.display_name,
-            task=self.task_id.id
+            project=self.project_id.display_name, task=self.task_id.id
         )
         return {
-            'company_id': self.company_id.id,
-            'journal_id': self._get_salary_journal().id,
-            'date': self.date,
-            'no_analytic_lines': True,
-            'ref': reference,
-            'line_ids': [
+            "company_id": self.company_id.id,
+            "journal_id": self._get_salary_journal().id,
+            "date": self.date,
+            "no_analytic_lines": True,
+            "ref": reference,
+            "line_ids": [
                 (5, 0),
                 (0, 0, self._get_wip_move_line_vals()),
                 (0, 0, self._get_salary_move_line_vals()),
@@ -144,7 +146,7 @@ class TimesheetLine(models.Model):
     def _create_wip_account_move(self):
         """Create the wip journal entry."""
         vals = self._get_wip_account_move_vals()
-        self.salary_account_move_id = self.env['account.move'].create(vals)
+        self.salary_account_move_id = self.env["account.move"].create(vals)
         self.salary_account_move_id.post()
 
     def _is_wip_account_move_reconciled(self):
@@ -157,16 +159,18 @@ class TimesheetLine(models.Model):
     def _update_wip_account_move(self):
         """Update the wip journal entry."""
         if self._is_wip_account_move_reconciled():
-            raise ValidationError(_(
-                'The timesheet line {description} can not '
-                'be updated because the work in progress entry ({move_name}) is already '
-                'transfered into the cost of goods sold.'
-            ).format(
-                description=_format_timesheet_line_description(self),
-                move_name=self.salary_account_move_id.name,
-            ))
+            raise ValidationError(
+                _(
+                    "The timesheet line {description} can not "
+                    "be updated because the work in progress entry ({move_name}) is already "
+                    "transfered into the cost of goods sold."
+                ).format(
+                    description=_format_timesheet_line_description(self),
+                    move_name=self.salary_account_move_id.name,
+                )
+            )
 
-        self.salary_account_move_id.state = 'draft'
+        self.salary_account_move_id.state = "draft"
         vals = self._get_wip_account_move_vals()
         self.salary_account_move_id.write(vals)
         self.salary_account_move_id.post()
@@ -174,15 +178,17 @@ class TimesheetLine(models.Model):
     def _reverse_salary_account_move_for_updated_timesheet(self):
         """Reverse the wip journal entry in the context of an updated timesheet."""
         if self._is_wip_account_move_reconciled():
-            raise ValidationError(_(
-                'The timesheet line {description} can not '
-                'be updated because the work in progress entry ({move_name}) would be '
-                'reversed. This journal entry was already transfered into '
-                'the cost of goods sold.'
-            ).format(
-                description=_format_timesheet_line_description(self),
-                move_name=self.salary_account_move_id.name,
-            ))
+            raise ValidationError(
+                _(
+                    "The timesheet line {description} can not "
+                    "be updated because the work in progress entry ({move_name}) would be "
+                    "reversed. This journal entry was already transfered into "
+                    "the cost of goods sold."
+                ).format(
+                    description=_format_timesheet_line_description(self),
+                    move_name=self.salary_account_move_id.name,
+                )
+            )
         self.salary_account_move_id.reverse_moves()
         self.salary_account_move_id = False
 
@@ -228,14 +234,7 @@ class TimesheetLine(models.Model):
 
         :rtype: Set
         """
-        return {
-            'name',
-            'amount',
-            'unit_amount',
-            'date',
-            'project_id',
-            'task_id',
-        }
+        return {"name", "amount", "unit_amount", "date", "project_id", "task_id"}
 
     @api.multi
     def write(self, vals):
@@ -256,14 +255,16 @@ class TimesheetLine(models.Model):
     def _reverse_salary_account_move_for_deleted_timesheet(self):
         """Reverse the wip journal entry in the context of a deleted timesheet."""
         if self._is_wip_account_move_reconciled():
-            raise ValidationError(_(
-                'The timesheet line {description} can not '
-                'be deleted because the work in progress entry ({move_name}) is already '
-                'transfered into the cost of goods sold.'
-            ).format(
-                description=_format_timesheet_line_description(self),
-                move_name=self.salary_account_move_id.name,
-            ))
+            raise ValidationError(
+                _(
+                    "The timesheet line {description} can not "
+                    "be deleted because the work in progress entry ({move_name}) is already "
+                    "transfered into the cost of goods sold."
+                ).format(
+                    description=_format_timesheet_line_description(self),
+                    move_name=self.salary_account_move_id.name,
+                )
+            )
         self.salary_account_move_id.reverse_moves()
 
     @api.multi
