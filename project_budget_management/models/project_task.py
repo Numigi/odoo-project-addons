76711c6203a15c658364275c7615838f41e365c6
cuong.nmtm <cuong.nmtm@komit-consulting.com>
2020-09-30 12:18:11 +0700

    Add translation for chatter messages

--- project_budget_management/models/project_task.py
+++ project_budget_management/models/project_task.py
@@ -10,36 +10,35 @@ class ProjectTask(models.Model):
     @api.model
     def create(self, vals):
         res = super().create(vals)
-        res.notify_template_task_changed_to_project("added")
+        res.notify_template_task_changed_to_project(_("added"))
         return res
 
     @api.multi
     def write(self, vals):
-        project_updated_template_tasks = []
         if "project_id" in vals:
             project_updated_template_tasks = self.filtered(
                 lambda r: r.project_id.id != vals["project_id"]
             )
             project_updated_template_tasks.notify_template_task_changed_to_project(
-                "removed from project"
+                _("removed from project")
             )
         res = super(ProjectTask, self).write(vals)
 
         if "project_id" in vals:
             project_updated_template_tasks.notify_template_task_changed_to_project(
-                "added"
+                _("added")
             )
         elif "min_hours" in vals or "planned_hours" in vals or "max_hours" in vals:
-            self.notify_template_task_changed_to_project("modified")
+            self.notify_template_task_changed_to_project(_("modified"))
         return res
 
     @api.multi
     def unlink(self):
-        self.notify_template_task_changed_to_project("deleted")
+        self.notify_template_task_changed_to_project(_("deleted"))
         return super().unlink()
 
     @api.multi
     def notify_template_task_changed_to_project(self, action):
-        message = "<ul><li>Task Template {} %s</li></ul>" % action
+        message = "<ul><li>%s</li></ul>" % (_("Task Template") + " {} " + action)
         for record in self.filtered(lambda r: r.is_template and r.project_id):
-            record.project_id.message_post(body=_(message.format(record.id)))
+            record.project_id.message_post(body=message.format(record.id))
