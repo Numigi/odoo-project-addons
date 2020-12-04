f980aeaa7328c5cbccada0091309bfd630a50642
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-12-04 09:59:07 -0500

    TA#22534 fix cases of task removed or deleted

--- project_budget_management/models/project_task.py
+++ project_budget_management/models/project_task.py
@@ -15,35 +15,55 @@ class ProjectTask(models.Model):
     @api.model
     def create(self, vals):
         res = super().create(vals)
-        res.notify_template_task_changed_to_project(ADDED)
+        res._project_budget_log_required().notify_template_task_added()
         return res
 
     @api.multi
     def write(self, vals):
         if "project_id" in vals:
-            project_updated_template_tasks = self.filtered(
+            added_tasks = self._project_budget_log_required().filtered(
                 lambda r: r.project_id.id != vals["project_id"]
             )
-            project_updated_template_tasks.notify_template_task_changed_to_project(
-                REMOVED
-            )
+            removed_tasks = [(t.project_id, t) for t in added_tasks]
         res = super(ProjectTask, self).write(vals)
 
         if "project_id" in vals:
-            project_updated_template_tasks.notify_template_task_changed_to_project(
-                ADDED
-            )
+            for project, task in removed_tasks:
+                task.notify_template_task_removed_from(project)
+
+            added_tasks.notify_template_task_added()
+
         elif "min_hours" in vals or "planned_hours" in vals or "max_hours" in vals:
-            self.notify_template_task_changed_to_project(MODIFIED)
+            self._project_budget_log_required().notify_template_task_modified()
+
         return res
 
     @api.multi
     def unlink(self):
-        self.notify_template_task_changed_to_project(DELETED)
-        return super().unlink()
+        deleted_tasks = [(t.project_id, t) for t in self._project_budget_log_required()]
+        res = super().unlink()
+        for project, task in deleted_tasks:
+            task.notify_template_task_deleted(project)
+        return res
+
+    def notify_template_task_added(self):
+        for task in self:
+            self.notify_template_task_changed_to_project(ADDED, task.project_id)
+
+    def notify_template_task_modified(self):
+        for task in self:
+            self.notify_template_task_changed_to_project(MODIFIED, task.project_id)
+
+    def notify_template_task_deleted(self, project):
+        for task in self:
+            self.notify_template_task_changed_to_project(DELETED, project)
+
+    def notify_template_task_removed_from(self, project):
+        for task in self:
+            self.notify_template_task_changed_to_project(REMOVED, project)
 
     @api.multi
-    def notify_template_task_changed_to_project(self, action):
+    def notify_template_task_changed_to_project(self, action, project):
         message_template = _(
             """
             <b>{action}</b>
@@ -54,15 +74,16 @@ class ProjectTask(models.Model):
             </ul>
         """
         )
-        for record in self.filtered(lambda r: r.is_template and r.project_id):
-            project = record.project_id
-            message = message_template.format(
-                action=self._format_template_task_action(action),
-                min=format_float_time(project.min_hours),
-                ideal=format_float_time(project.planned_hours),
-                max=format_float_time(project.max_hours),
-            )
-            record.project_id.message_post(body=message)
+        message = message_template.format(
+            action=self._format_template_task_action(action),
+            min=format_float_time(project.min_hours),
+            ideal=format_float_time(project.planned_hours),
+            max=format_float_time(project.max_hours),
+        )
+        project.message_post(body=message)
+
+    def _project_budget_log_required(self):
+        return self.filtered(lambda r: r.is_template and r.project_id)
 
     def _format_template_task_action(self, action):
         if action == ADDED:
