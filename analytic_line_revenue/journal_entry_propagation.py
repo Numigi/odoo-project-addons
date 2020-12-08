37ff21f1d2d8eb302f7407b15c8888fe32d51a05
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-12-08 10:20:47 -0500

    TA#29909 include other income in revenue analytic lines (#154)
    * TA#29909 include other income in revenue analytic lines
    
    * Fix code

--- analytic_line_revenue/journal_entry_propagation.py
+++ analytic_line_revenue/journal_entry_propagation.py
@@ -6,14 +6,16 @@ from odoo import models
 
 class AccountMoveLine(models.Model):
 
-    _inherit = 'account.move.line'
+    _inherit = "account.move.line"
 
     def create_analytic_lines(self):
         result = super().create_analytic_lines()
 
-        revenue_type = self.env.ref('account.data_account_type_revenue')
+        revenue_type = self.env.ref("account.data_account_type_revenue")
+        other_revenue_type = self.env.ref("account.data_account_type_other_income")
         revenue_lines = self.filtered(
-            lambda l: l.account_id.user_type_id == revenue_type)
-        revenue_lines.mapped('analytic_line_ids').write({'revenue': True})
+            lambda l: l.account_id.user_type_id in (revenue_type | other_revenue_type)
+        )
+        revenue_lines.mapped("analytic_line_ids").write({"revenue": True})
 
         return result
