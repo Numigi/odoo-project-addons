526fe35c480f821434a92fd887031585b9c5e7c1
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-12-08 10:03:20 -0500

    Fix code

--- analytic_line_revenue/journal_entry_propagation.py
+++ analytic_line_revenue/journal_entry_propagation.py
@@ -11,8 +11,10 @@ class AccountMoveLine(models.Model):
     def create_analytic_lines(self):
         result = super().create_analytic_lines()
 
+        revenue_type = self.env.ref("account.data_account_type_revenue")
+        other_revenue_type = self.env.ref("account.data_account_type_other_income")
         revenue_lines = self.filtered(
-            lambda l: l.account_id.user_type_id.internal_group == "income"
+            lambda l: l.account_id.user_type_id in (revenue_type | other_revenue_type)
         )
         revenue_lines.mapped("analytic_line_ids").write({"revenue": True})
 
