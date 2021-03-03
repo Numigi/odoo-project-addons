1b3e4310e4f07f0e2608ba07a478e30a4b1198de
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-03-03 11:40:50 -0500

    TA#29141 add budgeted hours tracking
    Initially, this feature was introduced in module project_budget_management.
    The later module is deprecated in favor of project_time_budget.

--- project_time_budget/models/project_task.py
+++ project_time_budget/models/project_task.py
@@ -7,6 +7,8 @@ ADDED = "added"
 MODIFIED = "modified"
 DELETED = "deleted"
 REMOVED = "removed"
+ARCHIVED = "archived"
+RESTORED = "restored"
 
 
 class ProjectTask(models.Model):
@@ -15,26 +17,46 @@ class ProjectTask(models.Model):
     @api.model
     def create(self, vals):
         res = super().create(vals)
-        res._project_budget_log_required().notify_template_task_added()
+        res._project_budget_log_required()._notify_template_task_added()
         return res
 
     @api.multi
     def write(self, vals):
-        if "project_id" in vals:
+        changing_project = "project_id" in vals
+        if changing_project:
             added_tasks = self._project_budget_log_required().filtered(
                 lambda r: r.project_id.id != vals["project_id"]
             )
             removed_tasks = [(t.project_id, t) for t in added_tasks]
+
+        archiving = "active" in vals and not vals["active"]
+        if archiving:
+            archived_tasks = self.filtered("active")
+
+        restoring = vals.get("active")
+        if restoring:
+            restored_tasks = self.filtered(lambda t: not t.active)
+
+        changing_estimates = (
+            "min_hours" in vals or "planned_hours" in vals or "max_hours" in vals
+        )
+
         res = super(ProjectTask, self).write(vals)
 
-        if "project_id" in vals:
+        if changing_project:
             for project, task in removed_tasks:
-                task.notify_template_task_removed_from(project)
+                task._notify_template_task_removed_from(project)
 
-            added_tasks.notify_template_task_added()
+            added_tasks._notify_template_task_added()
 
-        elif "min_hours" in vals or "planned_hours" in vals or "max_hours" in vals:
-            self._project_budget_log_required().notify_template_task_modified()
+        elif changing_estimates:
+            self._project_budget_log_required()._notify_template_task_modified()
+
+        elif archiving:
+            archived_tasks._project_budget_log_required()._notify_template_task_archived()
+
+        elif restoring:
+            restored_tasks._project_budget_log_required()._notify_template_task_restored()
 
         return res
 
@@ -43,27 +65,34 @@ class ProjectTask(models.Model):
         deleted_tasks = [(t.project_id, t) for t in self._project_budget_log_required()]
         res = super().unlink()
         for project, task in deleted_tasks:
-            task.notify_template_task_deleted(project)
+            task._notify_template_task_deleted(project)
         return res
 
-    def notify_template_task_added(self):
+    def _notify_template_task_added(self):
         for task in self:
-            self.notify_template_task_changed_to_project(ADDED, task.project_id)
+            task._notify_template_task_changed_to_project(ADDED, task.project_id)
 
-    def notify_template_task_modified(self):
+    def _notify_template_task_modified(self):
         for task in self:
-            self.notify_template_task_changed_to_project(MODIFIED, task.project_id)
+            task._notify_template_task_changed_to_project(MODIFIED, task.project_id)
 
-    def notify_template_task_deleted(self, project):
+    def _notify_template_task_deleted(self, project):
         for task in self:
-            self.notify_template_task_changed_to_project(DELETED, project)
+            task._notify_template_task_changed_to_project(DELETED, project)
 
-    def notify_template_task_removed_from(self, project):
+    def _notify_template_task_removed_from(self, project):
         for task in self:
-            self.notify_template_task_changed_to_project(REMOVED, project)
+            task._notify_template_task_changed_to_project(REMOVED, project)
 
-    @api.multi
-    def notify_template_task_changed_to_project(self, action, project):
+    def _notify_template_task_archived(self):
+        for task in self:
+            task._notify_template_task_changed_to_project(ARCHIVED, task.project_id)
+
+    def _notify_template_task_restored(self):
+        for task in self:
+            task._notify_template_task_changed_to_project(RESTORED, task.project_id)
+
+    def _notify_template_task_changed_to_project(self, action, project):
         message_template = _(
             """
             <b>{action}</b>
@@ -76,9 +105,9 @@ class ProjectTask(models.Model):
         )
         message = message_template.format(
             action=self._format_template_task_action(action),
-            min=format_float_time(project.min_hours),
-            ideal=format_float_time(project.planned_hours),
-            max=format_float_time(project.max_hours),
+            min=format_float_time(project.budget_min_hours),
+            ideal=format_float_time(project.budget_planned_hours),
+            max=format_float_time(project.budget_max_hours),
         )
         project.message_post(body=message)
 
@@ -92,6 +121,10 @@ class ProjectTask(models.Model):
             message = _("Task template #{} modified")
         elif action == REMOVED:
             message = _("Task template #{} removed from the project")
+        elif action == ARCHIVED:
+            message = _("Task template #{} archived")
+        elif action == RESTORED:
+            message = _("Task template #{} restored")
         else:
             message = _("Task template #{} deleted")
         return message.format(self.id)
