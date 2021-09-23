375de76275c735d7c6778addb2304036d9169661
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-09-23 07:44:58 -0400

    Add missing import

--- project_material/models/project_task.py
+++ project_material/models/project_task.py
@@ -1,6 +1,7 @@
 # © 2019 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
+from datetime import date
 from odoo import api, fields, models
 
 
