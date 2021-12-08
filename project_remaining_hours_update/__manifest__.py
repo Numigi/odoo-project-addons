82ad5a18382996f9d6407f44e3f4944eab6a625c
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2021-12-07 21:22:50 -0500

    Bump module version

--- project_remaining_hours_update/__manifest__.py
+++ project_remaining_hours_update/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Project Remaining Hours Update',
-    'version': '1.0.0',
+    'version': '1.1.0',
     'author': 'Numigi',
     'maintainer': 'Numigi',
     'website': 'https://bit.ly/numigi-com',
