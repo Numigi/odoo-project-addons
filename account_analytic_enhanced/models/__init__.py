fedfc0c4803fb1bbd5c58f6d825d2b0011e95ee1
Quentin Lavallée-Bourdeau <31708522+qtiplb@users.noreply.github.com>
2022-05-03 14:55:40 -0400

    TA#43967 add account_analytic_enhanced (#214)
    * Module was initially in odoo-account-addons (private repo) but needs 
    to be moved here since it is a dependency of  project_enhanced 
    (TA#43971)

---
+++ account_analytic_enhanced/models/__init__.py
@@ -0,0 +1,6 @@
+# © 2022 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import (
+    account_analytic_account,
+)
