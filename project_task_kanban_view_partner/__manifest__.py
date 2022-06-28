b4fe029a63062e2efcf2f4ef1d56c1b56eefc974
majouda <majdaelmariouli@gmail.com>
2022-06-28 15:13:47 +0100

    TA#44215 add module project_task_kanban_view_partner

---
+++ project_task_kanban_view_partner/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2022 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Project Task Kanban View Partner",
+    "version": "1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "LGPL-3",
+    "category": "Project",
+    "summary": "Replace email with customer in project task kanban view",
+    "depends": ["project"],
+    "data": [
+        "views/project_view.xml"
+    ],
+    "installable": True,
+}
