933d2574f61e2d918bdb919c271a12aa88619c5d
Abdellatif BENZBIRIA <a.benzbiria@magnetomedia.net>
2022-09-15 22:44:10 +0100

    TA#45452 [ADD] module project role milestone (#258)
    Co-authored-by: Abdellatif BENZBIRIA <abenzbiria@leyton.com>
    Co-authored-by: Julie LeBrun <38360781+TumbaoJu@users.noreply.github.com>

---
+++ project_role_milestone/models/project_milestone.py
@@ -0,0 +1,14 @@
+# © 2022 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+from odoo import models, fields
+
+
+class ProjectMilestone(models.Model):
+
+    _inherit = "project.milestone"
+
+    assignment_ids = fields.One2many(
+        "project.assignment",
+        "milestone_id",
+        "Milestone assignments",
+    )
