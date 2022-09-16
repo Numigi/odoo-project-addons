6772ae4157fd1bce67c1acd53c01903be4f007e8
Abdellatif BENZBIRIA <a.benzbiria@magnetomedia.net>
2022-09-16 18:36:38 +0100

    TA#45528[ADD] module project_milestone_responsible (#262)
    * TA#45528[ADD] module project_milestone_responsible
    
    * Update read me
    
    * Update Readme
    
    Co-authored-by: Abdellatif BENZBIRIA <abenzbiria@leyton.com>
    Co-authored-by: Julie LeBrun <38360781+TumbaoJu@users.noreply.github.com>

---
+++ project_milestone_responsible/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2022 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Project Milestone Responsible',
+    'version': '1.0.0',
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "LGPL-3",
+    "category": "Project",
+    'summary': "Assign a responsible for a project's milestone",
+    'depends': [
+        'project_milestone'
+    ],
+    'data': [
+          'views/project_milestone.xml',
+    ],
+    'installable': True,
+}
