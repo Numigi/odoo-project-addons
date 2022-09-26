5ca9e12dec4fd015776d7ca3586a0e4ca812564e
Abdellatif BENZBIRIA <a.benzbiria@magnetomedia.net>
2022-09-26 23:31:39 +0100

    TA#45528[ADD] module project_milestone_responsible (#271)
    * TA#45528 module project_milestone_responsible
    
    * TA#45528 module project_milestone_responsible
    
    Co-authored-by: Abdellatif BENZBIRIA <abenzbiria@leyton.com>
    Co-authored-by: Julie LeBrun <38360781+TumbaoJu@users.noreply.github.com>
    Co-authored-by: majouda <majdaelmariouli@gmail.com>

--- project_milestone_responsible/models/project_milestone.py
+++ project_milestone_responsible/models/project_milestone.py
@@ -1,6 +1,6 @@
-# © 2021 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# © 2022 - Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ProjectMilestone(models.Model):
@@ -10,6 +10,12 @@ class ProjectMilestone(models.Model):
     user_id = fields.Many2one(
         "res.users",
         "Responsible",
-        domain= [('share', '=', False)],
-        help="Assign a responsible by choosing an Internal user"
+        domain=[('share', '=', False)],
+        help="Assign a responsible by choosing an Internal User"
     )
+
+    @api.onchange("project_id")
+    def _onchange_project_id(self):
+        self.user_id = self.project_id.user_id.id if self.project_id else False
+
+
