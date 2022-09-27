5829c6badefe7099cdb570bfe7df0010b86d6ad7
majouda <majdaelmariouli@gmail.com>
2022-09-27 12:02:09 +0100

    TA#45451 Fix translation and code quality

--- project_milestone_start_date/__manifest__.py
+++ project_milestone_start_date/__manifest__.py
@@ -1,9 +1,9 @@
-# © 2022 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# © 2022 - Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     "name": "Project Milestone Start Date",
-    "version": "1.0.0",
+    "version": "1.0.1",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://bit.ly/numigi-com",
--- project_milestone_start_date/models/project_milestone.py
+++ project_milestone_start_date/models/project_milestone.py
@@ -1,11 +1,10 @@
-# © 2022 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# © 2022 - Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo import models, fields
 
 
 class ProjectMilestone(models.Model):
-
     _inherit = "project.milestone"
 
     start_date = fields.Date(string="Start Date")
