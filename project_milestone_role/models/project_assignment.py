c56339a5a5ef502c86edd873cd9d043d5b7f7587
Majda EL MARIOULI <majdaelmariouli@gmail.com>
2022-10-31 14:49:48 +0100

    TA#47583 Fix Translation (#283)

--- project_milestone_role/models/project_assignment.py
+++ project_milestone_role/models/project_assignment.py
@@ -1,6 +1,6 @@
-# © 2022 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# © 2022 - Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-from odoo import api, models, fields
+from odoo import api, models, fields, _
 
 
 class ProjectAssignment(models.Model):
@@ -34,13 +34,16 @@ class ProjectAssignment(models.Model):
     _sql_constraints = [
                         (
                            'project_milestone_role_user_uniq',
-                           'UNIQUE (project_id, role_id, user_id, milestone_id)',
-                           'User may be assigned per role and per milestone only once within a project!'
+                           'UNIQUE (project_id, role_id, '
+                           'user_id, milestone_id)',
+                           _('User may be assigned per role and per milestone '
+                             'only once within a project!')
                         ),
                         (
                             'project_role_user_uniq',
                             'Check(1=1)',
-                            'User may be assigned per role and per milestone only once within a project!'
+                            _('User may be assigned per role and per milestone'
+                              ' only once within a project!')
                         )
                         ]
 
@@ -54,13 +57,16 @@ class ProjectAssignment(models.Model):
         else:
             project_ids = self.env['project.project'].search([])
             if project_ids:
-                return {'domain': {'project_id': [('id', 'in', project_ids.ids)]}}
+                return {'domain': {'project_id':
+                                       [('id', 'in', project_ids.ids)]}}
 
     @api.onchange('project_id')
     def onchange_project(self):
         if self.project_id:
-            return {'domain': {'milestone_id': [('project_id', '=', self.project_id.id)]}}
+            return {'domain': {'milestone_id':
+                                   [('project_id', '=', self.project_id.id)]}}
         else:
             milestone_ids = self.env['project.milestone'].search([])
             if milestone_ids:
-                return {'domain': {'milestone_id': [('id', 'in', milestone_ids.ids)]}}
+                return {'domain': {'milestone_id':
+                                       [('id', 'in', milestone_ids.ids)]}}
