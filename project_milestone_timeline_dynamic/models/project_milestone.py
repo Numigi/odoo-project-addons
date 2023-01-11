741473ed2e1e496aa9f8fd7a08850c948a13c470
Majda EL MARIOULI <majdaelmariouli@gmail.com>
2023-01-11 10:29:47 -0500

    TA#45459 add constraint in start and end date (#298)
    * TA#45459 add constraint in start and end date
    
    * TA#45459 add constraint in start date of a milestone with dependencies
    
    * TA#45459 Fix constrains _check_date_start_before_date_end

--- project_milestone_timeline_dynamic/models/project_milestone.py
+++ project_milestone_timeline_dynamic/models/project_milestone.py
@@ -1,18 +1,36 @@
 # © 2022 - Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-from odoo import models, api
+from odoo import models, api, fields, _
 from datetime import timedelta
+from odoo.exceptions import ValidationError
 
 
 class ProjectMilestone(models.Model):
     _inherit = "project.milestone"
 
+    @api.constrains('start_date')
+    def _check_start_date_and_dependencies_end_date(self):
+        sorted_child_ids = self.child_ids.sorted(
+            key='target_date', reverse=True)
+        child_last_end_dates = self.child_ids.sorted(
+            key='target_date', reverse=True).mapped('target_date')
+        if child_last_end_dates and self.start_date and \
+                self.start_date < child_last_end_dates[0]:
+            raise ValidationError(
+                _('The milestone %s - %s is dependent on this '
+                  'milestone and has a %s end date.'
+                  % (sorted_child_ids[0].name,
+                     sorted_child_ids[0].project_id.name,
+                     fields.Date.to_string(child_last_end_dates[0]))
+                  )
+            )
+
     def _check_child_milestones_target_date(self, milestone):
         child_last_end_dates = milestone.child_ids.sorted(
             key='target_date', reverse=True).mapped('target_date')
-        if child_last_end_dates:
-            if not milestone.start_date or milestone.start_date <= \
-                    child_last_end_dates[0]:
+        if child_last_end_dates and \
+                (not milestone.start_date or milestone.start_date <= \
+                    child_last_end_dates[0]):
                 milestone_duration = \
                     milestone.target_date - milestone.start_date
                 milestone.start_date = \
