755ca3e3247c2986d02fde6c3cb1ae55f9e2a445
Houda BENTALEB <houda.bentaleb@gmail.com>
2023-02-15 15:11:11 +0100

    TA#50283 [FIX]project_task_stage_external_mail: TU (#306)
    * TA#50283 [FIX]project_task_stage_external_mail: TU
    
    * TA#50283 [ADD]: add module project_task_stage_external_mail to main manifest + DockerFile
    
    * TA#50283 [FIX] Code Quality
    
    ---------
    
    Co-authored-by: Majda EL MARIOULI <majdaelmariouli@gmail.com>

--- project_task_stage_external_mail/tests/test_project_task.py
+++ project_task_stage_external_mail/tests/test_project_task.py
@@ -1,4 +1,4 @@
-# © 2022 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo.tests import common
@@ -9,47 +9,67 @@ class TestProjectTask(common.SavepointCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        cls.user_demo = cls.env.ref('base.user_demo')
         cls.discussion_type = cls.env.ref('mail.mt_comment')
         cls.internal_note_type = cls.env.ref('mail.mt_note')
         cls.template = cls.env.ref('project_task_stage_external_mail.demo_mail_template_task_open')
-        cls.stage = cls.env['project.task.type'].create({
-            'name': 'New',
-            'mail_template_id': cls.template.id,
-            'external_mail': True,
-        })
-        cls.task = cls.env['project.task'].create({
-            'name': 'ttask',
-            'stage_id':  cls.stage.id,
-
+        cls.project = cls.env['project.project'].create({
+            'name': 'Project 1',
         })
         cls.external_partner = cls.env['res.partner'].create({
             'name': 'tpartner',
             'email': 'tpartner@example.com',
         })
-        cls.task.message_subscribe([cls.external_partner.id])
+        cls.project.message_subscribe([cls.external_partner.id])
 
-    def _get_sent_message(self):
-        message = self.task.message_ids.filtered(lambda m: 'Task Open: ' in (m.subject or ''))
-        return message
+        cls.stage1 = cls.env['project.task.type'].create({
+            'name': 'New',
+            'mail_template_id': cls.template.id,
+            'external_mail': False,
+            'project_ids': [(4, cls.project.id)],
+        })
+        cls.stage2 = cls.env['project.task.type'].create({
+            'name': 'Open',
+            'mail_template_id': cls.template.id,
+            'external_mail': True,
+            'project_ids': [(4, cls.project.id)],
+        })
 
-    def _get_message_recipients(self):
-        message = self._get_sent_message()
-        # The use of self.task._notify_thread(message) create the notification
+        cls.task1 = cls.env['project.task'].create({
+            'name': 'ttask 1',
+            'stage_id': cls.stage1.id,
+            'partner_id': cls.user_demo.partner_id.id,
+            'project_id': cls.project.id,
+            'allowed_user_ids': [(4, cls.user_demo.id)],
+
+        })
+        cls.task2 = cls.env['project.task'].create({
+            'name': 'ttask 2',
+            'stage_id': cls.stage2.id,
+            'partner_id': cls.user_demo.partner_id.id,
+            'project_id': cls.project.id,
+            'allowed_user_ids': [(4, cls.user_demo.id)],
+
+        })
+
+    def _get_sent_message(self, task):
+        messages = task.message_ids.filtered(lambda m: 'Task Open: ' in (m.subject or '')).sorted(key=lambda s: s.id,
+                                                                                                  reverse=True)
+        return messages
+
+    def _get_message_recipients(self, task):
+        message = self._get_sent_message(task)
         return message.mapped('notification_ids.res_partner_id')
 
-    def test_givenExternalMailIsUnchecked_thenInternalNoteSent(self):
-        self.stage.external_mail = False
-        assert self._get_sent_message().subtype_id == self.discussion_type
+    def test_givenExternalMailIsUnChecked_thenInternalNote(self):
+        assert self._get_sent_message(self.task1).subtype_id == self.internal_note_type
 
-    def test_givenExternalMailIsUnchecked_thenPartnerNotInRecipients(self):
-        # This test is OK only because of the empty resukt of _get_message_recipients
-        #TODO create a notification mail when subscribe a follower and check external_mail = False
-        self.stage.external_mail = False
-        assert self.external_partner not in self._get_message_recipients()
+    def test_givenExternalMailIsUnchecked_thenExternalPartnerNotInRecipients(self):
+        assert self._get_message_recipients(self.task1)
+        assert self.external_partner not in self._get_message_recipients(self.task1)
 
-    def test_givenExternalMailIsChecked_thenMessageSent(self):
-        assert self._get_sent_message().subtype_id == self.discussion_type
+    def test_givenExternalMailIsChecked_thenCommentSent(self):
+        assert self._get_sent_message(self.task2).subtype_id == self.discussion_type
 
-    # #TODO: Port this test method
-    # def test_givenExternalMailIsChecked_thenPartnerInRecipients(self):
-    #     assert self.external_partner in self._get_message_recipients()
+    def test_givenExternalMailIsChecked_thenExternalPartnerInRecipients(self):
+        assert self.external_partner in self._get_message_recipients(self.task2)
