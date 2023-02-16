d4b4bbba45943832846a35d4859be608cf7a35b5
Abdellatif BENZBIRIA <a.benzbiria@gmail.com>
2023-02-16 20:40:20 +0100

    TA#50481 [MIG][14.0]: port project_portal_hide_timesheets (#308)

---
+++ project_portal_hide_timesheets/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Project Portal Hide Timesheets',
+    'version': '12.0.0',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'website': 'https://bit.ly/numigi-com',
+    'license': 'LGPL-3',
+    'category': 'Project',
+    'depends': [
+        'hr_timesheet',
+    ],
+    'data': [
+        'views/portal.xml',
+    ],
+    'installable': True,
+}
