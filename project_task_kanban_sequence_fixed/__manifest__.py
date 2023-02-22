9774e4ae088ffe716a27499bbba4cec107db005f
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-02-22 11:08:02 +0300

    TA#50621 [ADD] project_task_kanban_sequence_fixed: locking column on project task kanban view

---
+++ project_task_kanban_sequence_fixed/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2019 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Project Task Kanban Sequence Fixed',
+    'version': '1.0.0',
+    'author': 'Numigi',
+    'maintainer': 'Numigi',
+    'license': 'LGPL-3',
+    'category': 'Other',
+    'summary': 'Lock the stage column drag and drop on kanban view',
+    'depends': [
+        'project',
+    ],
+    'data': [
+        'views/assets.xml',
+    ],
+    'installable': True,
+}
---
+++ project_task_kanban_sequence_fixed/static/src/js/kanban_column.js
@@ -0,0 +1,62 @@
+odoo.define('kanban_task_stage_lock.kanbanRenderer', function(require) {
+    "use strict";
+    var core = require('web.core');
+    var KanbanColumn = require("web.KanbanColumn");
+
+    var _t = core._t;
+
+    KanbanColumn.include({
+        start: function() {
+            this._super.apply(this, arguments);
+            var self = this;
+            var documentToNull = function() {
+                document.onmouseup = null;
+                document.mousemove = null;
+            };
+
+            document.onmouseover = myMouseOverHandler;
+            document.onmousedown = myMouseDownHandler;
+
+            // pointer prevents user that drag and drop not allowed
+            function myMouseOverHandler(event) {
+                var el = event.target
+                var condition = self.modelName === "project.task" &&
+                    event.button === 0 &&
+                    (el.classList.contains('o_column_unfold') ||
+                        el.classList.contains('o_column_title') ||
+                        event.target.classList.contains('fa-arrows-h'))
+                if (condition) {
+                    event.target.style.cursor = "not-allowed";
+                }
+            }
+
+            // handling pop-up message when moving column
+            function onMouseMove(event) {
+                document.removeEventListener('mousemove', onMouseMove);
+                documentToNull();
+                alert(_t("Oups! You can not sort columns order here in this view."));
+            }
+
+            // if user is trying to move column
+            function myMouseDownHandler(event) {
+                var el = event.target
+                var condition = self.modelName === "project.task" &&
+                    event.button === 0 &&
+                    (el.classList.contains('o_column_unfold') ||
+                        el.classList.contains('o_column_title') ||
+                        event.target.classList.contains('fa-arrows-h'))
+
+                if (condition) {
+                    event.target.click(function() {
+                        return false;
+                    });
+                    document.addEventListener('mousemove', onMouseMove);
+                    document.onmouseup = function() {
+                        document.removeEventListener('mousemove', onMouseMove);
+                        documentToNull();
+                    };
+                }
+            }
+        },
+    });
+});
