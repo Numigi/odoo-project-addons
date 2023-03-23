fdb232b3c3211c87c7198632c994cdc3a42f80f2
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-03-23 16:13:43 +0300

    TA#51653 [MIG] Migrating 2 modules for ISIDOR migration

---
+++ project_remaining_hours_update/init_hook.py
@@ -0,0 +1,32 @@
+# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+import logging
+
+from datetime import datetime
+from odoo import SUPERUSER_ID, fields
+from odoo.api import Environment
+
+_logger = logging.getLogger(__name__)
+
+
+def _log_current_remaining_hours(cr):
+    """Setup the field id_string on all tasks."""
+    env = Environment(cr, SUPERUSER_ID, {})
+    all_tasks = env['project.task'].search([])
+    number_of_tasks = len(all_tasks)
+    comment = 'Remaining hours on {}'.format(
+        fields.Date.to_string(datetime.now()))
+
+    for i, task in enumerate(all_tasks):
+        _logger.info(
+            'Logging current remaning hours on task ID={} ({} of {})'
+            .format(task.id, i, number_of_tasks)
+        )
+        task.update_remaining_hours(
+            task.remaining_hours, env.user, comment=comment
+        )
+
+
+def post_init_hook(cr, pool):
+    _log_current_remaining_hours(cr)
---
+++ project_template/tests/test_project.py
@@ -0,0 +1,97 @@
+# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo.tests import common
+
+
+class TestConvertTemplatesToTask(common.SavepointCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.stage_1 = cls.env['project.task.type'].create(
+            {'name': 'Stage 1', 'sequence': 1})
+        cls.stage_2 = cls.env['project.task.type'].create(
+            {'name': 'Stage 2', 'sequence': 2})
+
+        cls.project = cls.env['project.project'].create({
+            'name': 'Project A',
+            'type_ids': [(4, cls.stage_1.id), (4, cls.stage_2.id)],
+        })
+
+        cls.template_a = cls.env['project.task'].create({
+            'name': 'Template Task A1',
+            'is_template': True,
+            'stage_id': False,
+        })
+        cls.template_b = cls.env['project.task'].create({
+            'name': 'Template Task A2',
+            'is_template': True,
+            'stage_id': False,
+        })
+
+    def _get_effective_tasks(self):
+        return self.project.task_ids.filtered(lambda t: not t.is_template)
+
+    def test_one_task_created_per_template(self):
+        self.template_a.project_id = self.project
+        self.template_b.project_id = self.project
+        self.project.convert_templates_to_tasks()
+        effective_tasks = self._get_effective_tasks()
+        assert len(effective_tasks) == 2
+
+    def test_new_subtask_copied_under_new_task(self):
+        self.template_a.project_id = self.project
+        self.template_b.project_id = self.project
+        self.template_b.parent_id = self.template_a
+        self.project.convert_templates_to_tasks()
+        self._get_effective_tasks()
+        task_a = self.template_a.effective_task_ids
+        task_b = self.template_b.effective_task_ids
+        assert task_b.parent_id == task_a
+
+    def test_new_task_has_first_project_stage(self):
+        self.template_a.project_id = self.project
+        self.project.convert_templates_to_tasks()
+        task_a = self.template_a.effective_task_ids
+        assert task_a.stage_id == self.stage_1
+
+    def test_if_same_template_not_converted_twice(self):
+        self.template_a.project_id = self.project
+        self.project.convert_templates_to_tasks()
+        self.project.convert_templates_to_tasks()
+        effective_tasks = self._get_effective_tasks()
+        assert len(effective_tasks) == 1
+
+    def test_if_child_template_added_afterward__new_subtask_added(self):
+        self.template_a.project_id = self.project
+        self.project.convert_templates_to_tasks()
+
+        self.template_b.project_id = self.project
+        self.template_b.parent_id = self.template_a
+        self.project.convert_templates_to_tasks()
+
+        task_a = self.template_a.effective_task_ids
+        task_b = self.template_b.effective_task_ids
+        assert task_b.parent_id == task_a
+
+    def test_task_template_count(self):
+        self.template_a.project_id = self.project
+        self.template_b.project_id = self.project
+        assert self.project.task_count == 0
+        assert self.project.task_template_count == 2
+
+    def test_task_count(self):
+        self.template_a.project_id = self.project
+        self.template_b.project_id = self.project
+        self.template_a.is_template = False
+        self.template_b.is_template = False
+        assert self.project.task_count == 2
+        assert self.project.task_template_count == 0
+
+    def test_template_task_ids_compute(self):
+        self.template_a.project_id = self.project
+        self.template_b.project_id = self.project
+        self.project._compute_template_task_ids()
+        assert self.template_a in self.project.template_task_ids
+        assert self.template_b in self.project.template_task_ids
