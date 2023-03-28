4d0bb78b5aaaaab28f474c99cbd3b1dc977bfc70
houdaBENTALEB <houda.bentaleb@gmail.com>
2023-03-28 20:52:11 +0000

    TA#51392 [MIG] : project_form_with_dates+project_group_create+project_hide_create_sale_order+project_iteration_parent_only...

---
+++ project_milestone_enhanced/tests/test_project.py
@@ -0,0 +1,71 @@
+# © 2023 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo.tests.common import SavepointCase
+
+
+class TestProject(SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.project = cls.env["project.project"].create(
+            {"name": "My Project", "use_milestones": True}
+        )
+
+        cls.milestone = cls.env["project.milestone"].create(
+            {"name": "My Milestone", "project_id": cls.project.id}
+        )
+
+        cls.milestone_2 = cls.env["project.milestone"].create(
+            {"name": "My Milestone 2", "project_id": cls.project.id}
+        )
+
+        cls.task = cls.env["project.task"].create(
+            {
+                "name": "My Task",
+                "project_id": cls.project.id,
+                "milestone_id": cls.milestone.id,
+            }
+        )
+
+        cls.task_2 = cls.env["project.task"].create(
+            {
+                "name": "My Task 1",
+                "project_id": cls.project.id,
+                "milestone_id": cls.milestone.id,
+                "active": False,
+            }
+        )
+
+    def test_copy_project(self):
+        project = self.project.copy({})
+        tasks = project.with_context(active_test=False).task_ids
+        milestone = project.milestone_ids.filtered(
+            lambda milestone: not "2" in milestone.name
+        )
+        assert tasks[0].milestone_id == milestone and tasks[1].milestone_id == milestone
+
+    def test_copy_project_not_milestones(self):
+        project = self.project.with_context(milestones_no_copy=True).copy({})
+        assert not project.with_context(active_test=False).milestone_ids
+
+    def test_milestone_change_project(self):
+        new_project = self.project.copy({})
+        self.milestone.project_id = new_project.id
+        assert not self.milestone.project_task_ids
+
+    def test_project_change_use_milestones(self):
+        self.milestone_2.toggle_active()
+        self.project.use_milestones = False
+        assert not self.milestone.active
+        self.project.use_milestones = True
+        assert self.milestone.active
+        assert not self.milestone_2.active
+
+    def test_project_change_active(self):
+        self.milestone_2.toggle_active()
+        self.project.toggle_active()
+        assert not self.milestone.active
+        self.project.toggle_active()
+        assert self.milestone.active
+        assert not self.milestone_2.active
---
+++ project_milestone_spent_hours/models/account_analytic_line.py
@@ -0,0 +1,17 @@
+# © 2023 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class AccountAnalytic_line(models.Model):
+    _inherit = "account.analytic.line"
+
+    milestone_id = fields.Many2one(
+        "project.milestone",
+        related="task_id.milestone_id",
+        string="Milestone",
+        index=True,
+        compute_sudo=True,
+        store=True,
+    )
