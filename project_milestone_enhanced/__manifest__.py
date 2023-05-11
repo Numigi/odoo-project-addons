873d2edaacb9caffb9dba5a88aa6caad1363e5c0
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-05-11 07:32:43 +0300

    TA#52878 [FIX] project_milestone_enhanced : fix view for tasks

--- project_milestone_enhanced/__manifest__.py
+++ project_milestone_enhanced/__manifest__.py
@@ -2,7 +2,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Project Milestone Enhanced",
-    "version": "1.0.0",
+    "version": "1.1.0",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://bit.ly/numigi-com",
