ada1632f2ef2c7b06d1bcd5860e8f5f03a5e4c8e
Lanto R <124650562+lanto-razafindrabe@users.noreply.github.com>
2023-06-30 04:41:00 +0300

    TA#54614 [ADD] project_milestone_time_report_advanced : new module (#333)

---
+++ project_milestone_time_report_advanced/__manifest__.py
@@ -0,0 +1,22 @@
+# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Project Milestone Time Report Advanced",
+    "version": "1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "AGPL-3",
+    "category": "Project",
+    "depends": [
+        "project_milestone_time_report",
+        "project_milestone_time_kpi",
+        "project_stage",
+    ],
+    "summary": "Add new informations on Milestones time report.",
+    "data": [
+        "report/report.xml",
+    ],
+    "installable": True,
+}
