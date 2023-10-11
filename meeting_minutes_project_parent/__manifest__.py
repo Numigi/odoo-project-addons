c376659ba9b21b92aa1ac7d829f6268280bddcc0
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-10-11 12:47:38 +0300

    TA#56215 [ADD] meeting_minutes_project_parent : module base

---
+++ meeting_minutes_project_parent/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Meeting Minutes Project Parent",
+    "version": "1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "AGPL-3",
+    "category": "Project",
+    "depends": ["project_iteration"],
+    "summary": "Add some progress time on task",
+    "data": [
+        "views/_________________.xml",
+    ],
+    "installable": True,
+}
