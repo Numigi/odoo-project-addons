d5ad6096e43a257b165d382781a44cc51d92ae32
Lanto R <124650562+lanto-razafindrabe@users.noreply.github.com>
2023-11-29 01:09:46 +0300

    TA#58632 [IMP] project_track_end_date : improvements and new features (#345)
    * TA#58632 [IMP][ADD] project_track_end_date : improvements and new features
    
    ---------
    
    Co-authored-by: majouda <majdaelmariouli@gmail.com>

--- project_track_end_date/models/project_project.py
+++ project_track_end_date/models/project_project.py
@@ -2,54 +2,49 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models, _
-from odoo.exceptions import UserError
 
 
 class ProjectProject(models.Model):
     _inherit = "project.project"
 
-    stage_name = fields.Char(related="stage_id.name")
-    project_end_history_ids = fields.Many2many(
+    project_end_history_ids = fields.One2many(
         "project.end.history",
-        compute="_compute_project_end_history",
+        "project_id",
         string="Historics associated to this project",
     )
     project_end_history_count = fields.Integer(
         string="Historics",
         compute="_compute_project_end_history",
-        groups="project.group_project_user",
+        groups="project.group_project_manager",
     )
+    expected_week_duration = fields.Float(
+        string="Expected week duration",
+        compute="_compute_expected_week_duration",
+    )
+
+    def _compute_expected_week_duration(self):
+        for project in self:
+            project.expected_week_duration = (
+                (project.date - project.date_start).days / 7
+                if project.date and project.date_start
+                else 0
+            )
 
     def _compute_project_end_history(self):
         for project in self:
-            project.project_end_history_ids = self.env["project.end.history"].search(
-                [
-                    ("project_id", "=", project.id),
-                ]
-            )
-            project.project_end_history_count = len(project.project_end_history_ids)
-
-    @api.multi
-    def write(self, values):
-        # Actually state is many2one (or related with it)
-        # no external ID
-        if "date" in values and self and self.stage_name != "Prévu":
-            raise UserError(
-                _(
-                    "You can not modify the end date when project status is not 'Planned'."
-                )
-            )
-        return super(ProjectProject, self).write(values)
+            project.project_end_history_count =\
+                len(project.project_end_history_ids)
 
     @api.multi
     def action_edit_end_date(self):
         self.ensure_one()
-        action = self.env.ref("project_track_end_date.wizard_edit_end_date").read()[0]
+        action = self.env.ref("project_track_end_date.wizard_edit_end_date"
+                              ).read()[0]
         action["context"] = {
             "default_initial_date": self.date,
             "default_date": self.date,
             "default_project_id": self.id,
             "default_user_id": self.env.user.id,
-            "default_company_id": self.env.user.company_id.id,
+            "default_company_id": self.company_id.id,
         }
         return action
