22e63b948cb4592f1f986bc5815a89b7396fb3b1
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2024-02-06 11:31:46 +0100

    TA#57567[ADD] : new module project_iteration_forecasted_end_date

--- project_iteration_forecasted_end_date/__manifest__.py
+++ project_iteration_forecasted_end_date/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Project Iteration Forecasted End Date',
-    'version': '1.0.1',
+    'version': '1.0.0',
     'author': 'Numigi',
     'maintainer': 'Numigi',
     'website': 'https://bit.ly/numigi-com',
