3aa810a40af6dc33ad5599121d71bf1de122afee
majouda <majdaelmariouli@gmail.com>
2024-02-07 10:30:44 -0500

    Improve Code Quality

--- project_material/models/project_task_material.py
+++ project_material/models/project_task_material.py
@@ -3,7 +3,6 @@
 
 from datetime import timedelta
 from odoo import api, fields, models, _
-from odoo.addons import decimal_precision as dp
 from odoo.exceptions import ValidationError
 from odoo.tools import float_compare
 
