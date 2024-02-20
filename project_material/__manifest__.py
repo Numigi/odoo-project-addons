72a804a4e29bdd142da82485d7c34e57b77cbec0
majouda <majdaelmariouli@gmail.com>
2024-02-20 16:16:58 -0500

    TA#60275 [FIX] Adapt multi-company ir_rule to V14

--- project_material/__manifest__.py
+++ project_material/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Project Material",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.2.0",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://bit.ly/numigi-com",
