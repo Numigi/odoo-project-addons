f57b607e15f4f06eb7184037fe4a87c3fe022102
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2024-03-26 15:21:12 +0300

    TA#61352 [MIG] project_invoicing_profile : migration to 14.0

---
+++ project_invoicing_profile/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2024 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Project Invoicing Profile",
+    "version": "14.0.1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "license": "LGPL-3",
+    "category": "Project",
+    "summary": "Add invoicing profiles to projects",
+    "depends": ["project", "hr_timesheet"],
+    "data": [
+        "security/groups.xml",
+        "views/project_invoice_profile.xml",
+        "views/project_project.xml",
+        "security/ir.model.access.csv",
+    ],
+    "installable": True,
+}
