6a72c80e9fc8bb4779855f1f899aebda76c85b91
Lanto R <124650562+lanto-razafindrabe@users.noreply.github.com>
2024-05-23 23:27:21 +0300

    TA#64810 [ADD] project_advanced_checklist (#391)
    * TA#64810 [ADD] project_advanced_checklist

---
+++ project_advanced_checklist/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2024 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Project Advanced Checklist",
+    "version": "1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "AGPL-3",
+    "category": "Project",
+    "depends": ["project"],
+    "summary": "Add advanced checklist in tasks",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/project_checklist.xml",
+        "views/project_task.xml",
+    ],
+    "installable": True,
+}
