2047b70dd3c33c942051433e4cdae1ee7f8761a2
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2024-06-28 03:31:56 +0300

    [ADD] Init module folder from 14.0

---
+++ project_task_stage_external_mail/models/project_task.py
@@ -0,0 +1,24 @@
+# © 2023 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+import logging
+from odoo import models, fields
+
+_logger = logging.getLogger(__name__)
+
+
+class ProjectTaskTypeExternalMail(models.Model):
+    _inherit = 'project.task.type'
+
+    external_mail = fields.Boolean()
+
+
+class ProjectTask(models.Model):
+    _inherit = 'project.task'
+
+    def _track_template(self, changes):
+        res = super(ProjectTask, self)._track_template(changes)
+        test_task = self[0]
+        if 'stage_id' in res and test_task.stage_id.external_mail:
+            res['stage_id'][-1]['subtype_id'] = self.env['ir.model.data'].xmlid_to_res_id(
+                'mail.mt_comment')
+        return res
