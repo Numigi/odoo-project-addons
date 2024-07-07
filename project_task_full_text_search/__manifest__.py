647b9a6eb4956f14ea9f488644c92185ca7c3fae
Lanto R <124650562+lanto-razafindrabe@users.noreply.github.com>
2024-07-07 19:07:27 +0300

    TA#66862 [16.0][MIG] project_task_full_text_search (#406)
    * TA#66862 [16.0][MIG] project_task_full_text_search
    
    ---------
    
    Co-authored-by: Majda EL MARIOULI <majdaelmariouli@gmail.com>

---
+++ project_task_full_text_search/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2024-today Numigi and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Project Task Full Text Search",
+    "version": "16.0.1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "LGPL-3",
+    "category": "Project",
+    "depends": ["project"],
+    "external_dependencies": {
+        "python": ["unidecode"],
+    },
+    "data": [
+        "views/project_task.xml",
+    ],
+    "installable": True,
+}
---
+++ project_task_full_text_search/models/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2024-today Numigi and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import (
+    ir_config_parameter,
+    project_task,
+)
