3a8a9377bd1874f7da9f1a6ed9fcb7080ba85f8b
Majda EL MARIOULI <majdaelmariouli@gmail.com>
2024-08-13 10:12:50 -0400

    TA#56214 [IMP] meeting_minutes_project (#412)

--- meeting_minutes_project/models/meeting_minutes_project.py
+++ meeting_minutes_project/models/meeting_minutes_project.py
@@ -1,75 +1,100 @@
 # © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from datetime import datetime
 from pytz import timezone
 from odoo import fields, models, api, _
 
 
 class MeetingMinutesProject(models.Model):
     _name = "meeting.minutes.project"
-    _inherit = ["mail.thread", "mail.activity.mixin", "meeting.minutes.mixin"]
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _inherits = {"meeting.minutes.mixin": "meeting_minute_id"}
 
-    name = fields.Char(compute="_compute_name")
     task_id = fields.Many2one(
-        "project.task", string="Task", ondelete="restrict", index=True, store=True
+        "project.task",
+        string="Task",
+        ondelete="restrict",
     )
     project_id = fields.Many2one(
         "project.project",
-        related="task_id.project_id",
         string="Project",
-        readonly=True,
-        store=True,
     )
-    action_ids = fields.One2many(
-        "mail.activity", compute="_compute_action_ids", string="Actions", readonly=True
+    meeting_minute_id = fields.Many2one(
+        "meeting.minutes.mixin",
+        string='Meeting Minute',
+        required=True,
+        ondelete='cascade'
     )
     discuss_point_ids = fields.One2many(
-        "meeting.minutes.discuss.point", "meeting_minutes_id", string="Discussed Points"
+        "meeting.minutes.discuss.point",
+        "meeting_minutes_id",
+        string="Discussed Points"
+    )
+    action_ids = fields.One2many(
+        "mail.activity",
+        compute="_compute_action_ids",
+        string="Actions", readonly=True
     )
     homework_ids = fields.One2many(
-        "mail.activity", "meeting_minutes_id", string="Homework"
+        "mail.activity",
+        "meeting_minutes_id",
+        string="Homework"
     )
+
+    def _set_meeting_minutes_name(self, record):
+        self.ensure_one()
+        name_format = _("Meeting Minutes: {record_name} - {create_datetime}")
+        create_date = self.create_date or datetime.today()
+        create_date = create_date.astimezone(timezone(self.env.user.tz))
+        self.name = name_format.format(
+            record_name=record.display_name,
+            create_datetime=create_date.strftime("%Y-%m-%d %H:%M:%S"),
+        )
+
+    def _set_document_ref(self, record, model):
+        self.ensure_one()
+        self.res_id = record.id
+        self.res_model = model
+
+    def _set_attendees(self, record):
+        self.ensure_one()
+        # Filter odoobot to avoid displaying it on edit mode then disappear on save
+        odoobot_id = self.env.ref("base.partner_root")
+        partner_follower_ids = record.message_follower_ids.filtered(
+                lambda f: f.partner_id
+                and not f.channel_id
+                and not f.partner_id.is_company
+                and f.partner_id.id != odoobot_id
+            )
+        self.partner_ids = [
+            (6, 0, [follower.partner_id.id for follower in partner_follower_ids])
+        ]
 
     @api.onchange("task_id")
     def on_change_task_id(self):
-        # Filter odoobot to avoid displaying it on edit mode then disappear on save
-        odoobot_id = self.env["ir.model.data"].xmlid_to_res_id("base.partner_root")
-        partner_follower_ids = self.task_id.message_follower_ids.filtered(
-            lambda f: f.partner_id
-            and not f.channel_id
-            and not f.partner_id.is_company
-            and f.partner_id.id != odoobot_id
-        )
-        self.partner_ids = [
-            (
-                6,
-                0,
-                [follower.partner_id.id for follower in partner_follower_ids],
-            )
-        ]
+        if self.task_id:
+            self._set_attendees(self.task_id)
+            self.project_id = self.task_id.project_id.id
+            self._set_document_ref(self.task_id, "project.task")
+            self._set_meeting_minutes_name(self.task_id)
 
-    def _compute_name(self):
-        name_format = _("Meeting Minutes: {task} - {create_datetime}")
-        for record in self:
-            date = record.create_date.astimezone(timezone(self.env.user.tz))
-            record.name = name_format.format(
-                task=record.task_id.display_name,
-                create_datetime=date.strftime("%Y-%m-%d %H:%M:%S"),
-            )
+    @api.onchange("project_id")
+    def on_change_project_id(self):
+        if self.project_id and not self.task_id:
+            self._set_attendees(self.project_id)
+            self._set_document_ref(self.project_id, "project.project")
+            self._set_meeting_minutes_name(self.project_id)
 
     @api.multi
     def _compute_action_ids(self):
         homework = self.env.ref("meeting_minutes_project.activity_homework")
         today = fields.Date.context_today(self)
-
         for rec in self:
-            activities = self.env["mail.activity"].search(
-                [("res_id", "in", rec.task_id.project_id.task_ids.ids)],
-            )
-            rec.action_ids = activities.filtered(
-                lambda a: (
-                    a.activity_type_id == homework
-                    and a.res_model == "project.task"
-                    and a.date_deadline < today
-                )
-            )
+            activities = self.env["mail.activity"].search([
+                ("res_model", "=", "project.task"),
+                ("res_id", "in", rec.project_id.task_ids.ids),
+                ("activity_type_id", "=", homework.id),
+                ("date_deadline", "<", today),
+            ])
+            rec.action_ids = activities
