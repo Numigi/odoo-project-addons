76e6ef852ec5d9c9fcbe95aee683d569c91f0d2f
Rivo Lalaina <rivo2302@gmail.com>
2024-11-22 15:28:51 +0300

    [MIG][16.0] project_analytic_group

---
+++ project_analytic_group/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2024-today Numigi and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Project Analytic Group",
+    "version": "14.0.1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "LGPL-3",
+    "category": "Project",
+    "summary": "Add analytic groups on projects and tasks",
+    "depends": ["hr_timesheet"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/project_project_views.xml",
+        "views/project_task_views.xml",
+    ],
+    "installable": True,
+}
