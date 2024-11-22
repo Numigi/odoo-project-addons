d512ca38924fd4bbf86577116758f8f478344dea
Rivo Lalaina <rivo2302@gmail.com>
2024-11-22 17:00:48 +0300

    [MIG][16.0] project_task_description_template

---
+++ project_task_description_template/models/project_task_description_template.py
@@ -0,0 +1,16 @@
+# Copyright 2024-today Numigi and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ProjectTaskDescriptionTemplate(models.Model):
+
+    _name = "project.task.description.template"
+    _description = "Project Task Description Template"
+    _order = "sequence"
+
+    name = fields.Char(required=True)
+    description = fields.Html()
+    active = fields.Boolean(default=True)
+    sequence = fields.Integer()
