30941a3387d383cb725561769ffe72fa211f835a
Rivo Lalaina <rivo2302@gmail.com>
2024-11-25 15:45:41 +0300

    [MIG][16.0] project_closed

---
+++ project_closed/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2024 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Project Stage Closed",
+    "version": "16.0.1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "LGPL-3",
+    "category": "Project",
+    "summary": "Define project stages as closed",
+    "depends": ["project"],
+    "data": [
+        "data/data.xml",
+        "views/project_project.xml",
+        "views/project_stage_views.xml",
+    ],
+    "installable": True,
+}
---
+++ project_closed/models/project_stage.py
@@ -0,0 +1,11 @@
+# Copyright 2024 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class ProjectStage(models.Model):
+
+    _inherit = "project.project.stage"
+
+    closed = fields.Boolean()
