e879805e211887902b4cf0961eea3fd38397e22d
Rivo Lalaina <rivo2302@gmail.com>
2024-11-25 15:49:59 +0300

    [MIG][16.0] project_closed

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -12,6 +12,7 @@
     "summary": "Install all addons required for testing.",
     "depends": [
         "project",
+        "project_closed",
         "project_task_date_planned",
         "project_task_full_text_search",
         "project_type_advanced",
