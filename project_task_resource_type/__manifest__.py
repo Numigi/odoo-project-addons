452e16d8e3f17cbd5204397d7edc5754c525ca47
Rivo Lalaina <rivo2302@gmail.com>
2024-11-26 14:45:24 +0300

    [MIG][16.0] project_task_ressource_type

---
+++ project_task_resource_type/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2024 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Project Task Resource Type",
+    "version": "16.0.1.0.0",
+    "description": "Add the referentiel Resource and the field to the tasks.",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "LGPL-3",
+    "category": "Project",
+    "depends": ["project"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/project_resource_views.xml",
+        "views/project_task_views.xml",
+    ],
+    "application": False,
+    "license": "LGPL-3",
+    "installable": True,
+}
