717303dee90287d784ca0b72e3473c9d3f3077bb
Rivo Lalaina <rivo2302@gmail.com>
2024-11-27 13:46:10 +0300

    [MIG][16.0]  project_task_subtask_same_project

---
+++ project_task_subtask_same_project/README.rst
@@ -0,0 +1,22 @@
+Project Task Subtask Same Project
+=================================
+Force the subtasks to be on the same project than its parent.
+
+
+When attempting to assign a subtask to a task from a different project, 
+an error message is displayed
+
+.. image:: static/description/subtask_in_different_project.png
+
+Project Readonly on Subtask
+---------------------------
+On the form view of a subtask, the project is readonly.
+The value is automatically propagated from the parent task.
+
+.. image:: static/description/subtask_project_readonly.png
+
+
+
+Contributors
+------------
+* Numigi (tm) and all its contributors (https://bit.ly/numigiens)
---
+++ project_task_subtask_same_project/tests/test_project_task.py
@@ -0,0 +1,51 @@
+# Copyright 2024 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+import pytest
+from odoo.exceptions import ValidationError
+from odoo.tests.common import TransactionCase
+
+
+class TestProjectTaskSubTaskSameProject(TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.project_a = cls.env["project.project"].create({"name": "projectA"})
+        cls.project_b = cls.env["project.project"].create({"name": "projectB"})
+        cls.task_parent = cls.env["project.task"].create(
+            {"name": "Task Parent", "project_id": cls.project_a.id}
+        )
+        cls.subtask_1 = cls.env["project.task"].create(
+            {
+                "name": "Task Child 1",
+                "project_id": cls.task_parent.project_id.id,
+                "parent_id": cls.task_parent.id,
+                "planned_hours": 1.0,
+            }
+        )
+        cls.subtask_2 = cls.env["project.task"].create(
+            {
+                "name": "Task Child 2",
+                "project_id": cls.task_parent.project_id.id,
+                "parent_id": cls.task_parent.id,
+                "planned_hours": 1.0,
+            }
+        )
+
+    def test_whenParentTaskChangeProject_thenSubTaskInheritNewProject(self):
+        """
+        Given a parent task is on project A
+        And the subtasks are on project A too
+
+        When the parent task is changed to project B
+
+        Then the subtasks are changed to project B too.
+        """
+        self.task_parent.project_id = self.project_b.id
+
+        assert self.subtask_1.project_id == self.project_b
+        assert self.subtask_2.project_id == self.project_b
+
+    def test_onUpdateSubtask_ifNotSameProject_raiseError(self):
+        with pytest.raises(ValidationError):
+            self.subtask_1.project_id = self.project_b
