f67333c5058d49129c550406d30f76a7ec147a30
Rivo Lalaina <rivo2302@gmail.com>
2024-11-28 08:46:25 +0300

    [MIG][16.0] project_task_editable_list_view

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -13,6 +13,7 @@
     "depends": [
         "project",
         "project_task_date_planned",
+        "project_task_editable_list_view",
         "project_task_full_text_search",
         "project_type_advanced",
         "project_default_task_stage",
