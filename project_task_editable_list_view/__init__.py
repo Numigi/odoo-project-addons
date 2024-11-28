35a537e935b088f987fb3adf0645ef8e19cd0ae8
Rivo Lalaina <rivo2302@gmail.com>
2024-11-28 08:45:02 +0300

    [MIG][16.0] project_task_editable_list_view

---
+++ project_task_editable_list_view/__init__.py
@@ -0,0 +1,2 @@
+# Copyright 20224 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
---
+++ project_task_editable_list_view/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 20224 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Project Task Editable List",
+    "version": "16.0.1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "LGPL-3",
+    "category": "Project",
+    "summary": "Add an editable list view of tasks",
+    "depends": ["project_task_date_planned"],
+    "data": ["views/project_task_views.xml", "views/menu_items.xml"],
+    "installable": True,
+}
