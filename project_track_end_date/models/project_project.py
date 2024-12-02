163143afd04082e5ce72498d336e80909ab6f8e3
Lanto R. <124650562+lanto-razafindrabe@users.noreply.github.com>
2024-12-02 10:14:20 +0300

    [IMP] Code improvement

--- project_track_end_date/models/project_project.py
+++ project_track_end_date/models/project_project.py
@@ -1,7 +1,7 @@
 # Copyright 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models, _
+from odoo import fields, models
 
 
 class ProjectProject(models.Model):
