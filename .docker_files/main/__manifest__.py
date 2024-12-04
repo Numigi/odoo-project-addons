5343898d2fa7baf5c3f70b93a3112ce11467d189
Lanto R. <124650562+lanto-razafindrabe@users.noreply.github.com>
2024-12-04 16:50:54 +0300

    TA#72164 [16.0][MIG] project_milestone_estimated_hours

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -12,6 +12,7 @@
     "summary": "Install all addons required for testing.",
     "depends": [
         "project",
+        "project_milestone_estimated_hours",
         "project_task_date_planned",
         "project_task_full_text_search",
         "project_type_advanced",
