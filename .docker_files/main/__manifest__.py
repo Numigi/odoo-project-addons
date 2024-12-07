005ad6b942c45b911048e4b228981d4a90a9b285
Lanto R. <124650562+lanto-razafindrabe@users.noreply.github.com>
2024-12-07 14:12:08 +0300

    TA#72198 [16.0][MIG] project_task_milestone_mandatory

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -16,6 +16,7 @@
         "project_task_full_text_search",
         "project_type_advanced",
         "project_default_task_stage",
+        "project_task_milestone_mandatory",
         "project_stage_allow_timesheet",
     ],
     "installable": True,
