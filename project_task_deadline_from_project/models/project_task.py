4acc1f924ef83efa65a4a512609a5543e2357a57
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2024-12-09 14:32:21 +0100

    [16.0][MIG] project_task_deadline_from_project

--- project_task_deadline_from_project/models/project_task.py
+++ project_task_deadline_from_project/models/project_task.py
@@ -1,4 +1,4 @@
-# Copyright2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# Copyright 2018-today  Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo import api, models
