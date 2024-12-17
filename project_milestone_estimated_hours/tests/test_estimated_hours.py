00571276cb342ac4749aae01491fcbc1172e6f3d
Lanto Razafindrabe <124650562+lanto-razafindrabe@users.noreply.github.com>
2024-12-17 13:30:50 +0300

    TA#72164 [16.0][MIG] project_milestone_estimated_hours (#474)
    * TA#72164 [16.0][MIG] project_milestone_estimated_hours
    
    * TA#72164 [16.0][MIG] project_milestone_estimated_hours
    
    * TA#72164 [16.0][MIG] project_milestone_estimated_hours
    
    * Update test_estimated_hours.py
    
    * [UPD] Update Dockerfile
    
    * [IMP] Update Dockerfile
    
    * [IMP] Update __manifest__.py
    
    * TA#72164 [16.0][MIG][FIX] project_milestone_estimated_hours : mandatory field
    
    ---------
    
    Co-authored-by: Abdellatif BENZBIRIA <a.benzbiria@gmail.com>

---
+++ project_milestone_estimated_hours/tests/test_estimated_hours.py
@@ -0,0 +1,25 @@
+# Copyright 2023 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo.tests.common import TransactionCase
+
+
+class TestEstimatedHours(TransactionCase):
+    def setUp(self):
+        super().setUp()
+        self.project = self.env["project.project"].create(
+            {"name": "My Project", "allow_milestones": True}
+        )
+        self.milestone = self.env["project.milestone"].create(
+            {
+                "name": "My Milestone",
+                "estimated_hours": 10,
+                "project_id": self.project.id,
+            }
+        )
+        self.milestone_copy = self.milestone.copy()
+
+    def test_estimated_hours_copy(self):
+        assert (
+            self.milestone.estimated_hours == self.milestone_copy.estimated_hours == 10
+        )
