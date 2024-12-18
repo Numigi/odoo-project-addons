e48baa08bce4b667ae12440ed1254a205131fe4f
Lanto R. <124650562+lanto-razafindrabe@users.noreply.github.com>
2024-12-18 10:27:22 +0300

    Init module in 16.0 from 14.0

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -11,12 +11,12 @@
     "category": "Other",
     "summary": "Install all addons required for testing.",
     "depends": [
-        "project",
+        "project_default_task_stage",
+        "project_stage_allow_timesheet",
+        "project_task_analytic_lines",
         "project_task_date_planned",
         "project_task_full_text_search",
         "project_type_advanced",
-        "project_default_task_stage",
-        "project_stage_allow_timesheet",
     ],
     "installable": True,
 }
