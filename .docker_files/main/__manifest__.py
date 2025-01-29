dfc690ac8c3e9ab2839838dfcae283a3c4a430bb
Rivo Lalaina <rivo2302@gmail.com>
2025-01-29 16:38:33 +0300

    [16.0] [MIG] project_parent_forecasted_end_date

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -20,6 +20,7 @@
         "project_milestone_time_kpi",
         "project_no_quick_create",
         "project_parent_enhanced",
+        "project_parent_forecasted_end_date",
         "project_portal_hide_timesheets",
         "project_progress_variance",
         "project_projected_hours",
