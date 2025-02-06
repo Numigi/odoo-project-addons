e31972a821db0cdae56417d6ce516b368da5c936
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2025-02-06 13:54:01 +0100

    [16.0][MIG] project_task_analytic_lines

--- project_task_analytic_lines/tests/test_account_move_propagation.py
+++ project_task_analytic_lines/tests/test_account_move_propagation.py
@@ -7,6 +7,6 @@ from .common import InvoiceCase
 class TestTaskPropagationOnTimesheet(InvoiceCase):
     def test_on_post__origin_task_propagated_to_analytic_lines(self):
         self._validate_invoice()
-        print(self.invoive.invoice_line_ids)
+        print(self.invoice.invoice_line_ids)
         line = self.invoice.mapped("invoice_line_ids.analytic_line_ids")
         assert line.origin_task_id == self.task
