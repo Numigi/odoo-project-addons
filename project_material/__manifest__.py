74fdcd8c60b5de341b38c090321668222e3b8fe2
majouda <majdaelmariouli@gmail.com>
2025-02-06 18:32:40 -0500

    TA#74336 [14.0][IMP] project_material

--- project_material/__manifest__.py
+++ project_material/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Project Material",
-    "version": "14.0.1.4.1",
+    "version": "14.0.1.5.0",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://bit.ly/numigi-com",
