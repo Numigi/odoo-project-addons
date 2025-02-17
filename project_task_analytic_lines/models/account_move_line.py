0978126e24578d635b26bde2d14cb8635a71115d
Abdellatif BENZBIRIA <a.benzbiria@gmail.com>
2025-02-17 19:29:14 +0100

    TA#72185 [16.0][MIG] project_task_analytic_lines (#513)
    * Init module in 16.0 from 14.0
    
    * TA#72185 [16.0][MIG] project_task_analytic_lines
    
    * Update fr.po
    
    * [16.0][MIG] project_task_analytic_lines
    
    * [16.0][MIG] project_task_analytic_lines
    
    * [16.0][MIG] project_task_analytic_lines
    
    * [16.0][MIG] project_task_analytic_lines
    
    * [16.0][MIG] project_task_analytic_lines
    
    * [16.0][MIG] project_task_analytic_lines
    
    * [16.0][MIG] project_task_analytic_lines
    
    * [16.0][MIG] project_task_analytic_lines
    
    * [16.0][MIG] project_task_analytic_lines
    
    * [16.0][MIG] project_task_analytic_lines
    
    * [16.0][MIG] project_task_analytic_lines
    
    * [16.0][MIG] project_task_analytic_lines
    
    * [16.0][MIG] project_task_analytic_lines
    
    * [16.0][MIG] project_task_analytic_lines
    
    * [16.0][MIG] project_task_analytic_lines
    
    * [16.0][MIG] project_task_analytic_lines
    
    * TA#72185 [16.0][MIG][CHG] project_task_analytic_lines : account_analytic_line
    
    * TA#72185 [16.0][MIG][CHG] project_task_analytic_lines : account_move_line
    
    * Minor changes
    
    * Update account_move_line_views.xml
    
    ---------
    
    Co-authored-by: Lanto R. <124650562+lanto-razafindrabe@users.noreply.github.com>
    Co-authored-by: Rivo Lalaina <rivo2302@gmail.com>

---
+++ project_task_analytic_lines/models/account_move_line.py
@@ -0,0 +1,66 @@
+# Copyright 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    task_id = fields.Many2one(
+        "project.task",
+        string="Task",
+        ondelete="restrict",
+        index=True,
+    )
+
+    analytic_distribution_ids = fields.Many2many(
+        "account.analytic.account",
+        compute="_compute_analytic_distribution_ids",
+        store=True,
+        string="Analytic Distribution IDs",
+    )
+
+    @api.depends("analytic_distribution")
+    def _compute_analytic_distribution_ids(self):
+        for rec in self:
+            if rec.analytic_distribution:
+                rec.analytic_distribution_ids = [
+                    (6, 0, list(map(int, rec.analytic_distribution.keys())))
+                ]
+            else:
+                rec.analytic_distribution_ids = [(6, 0, [])]
+
+    @api.onchange("analytic_distribution")
+    def _onchange_analytic_account_empty_task(self):
+        if (
+            self.task_id.project_id.analytic_account_id.id
+            not in self.analytic_distribution_ids.ids
+        ):
+            self.task_id = False
+
+    def _prepare_analytic_line(self):
+        result = super()._prepare_analytic_line()
+        for vals in result:
+            move_line = self.browse(vals["move_id"])
+            vals["origin_task_id"] = move_line.task_id.id
+        return result
+
+    def _check_task_matches_with_project(self):
+        task_not_matching_project = (
+            self.task_id
+            and self.task_id.project_id.analytic_account_id.id
+            not in self.analytic_distribution_ids.ids
+        )
+        if task_not_matching_project:
+            raise ValidationError(
+                _(
+                    "The task {task} is set on the invoice line {line}. "
+                    "This task does not correspond to the project IDs {project} set on the line"
+                ).format(
+                    line=self.display_name,
+                    task=self.task_id.display_name,
+                    project=self.analytic_distribution_ids.ids,
+                )
+            )
