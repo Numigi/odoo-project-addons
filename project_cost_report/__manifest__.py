4c5e2c523ec69c771b8ebcec4ee02ccb73f54038
Rivo Lalaina <rivo2302@gmail.com>
2025-02-17 13:22:28 +0300

    [14.0][FIX]] project_cost_report (#519)

--- project_cost_report/__manifest__.py
+++ project_cost_report/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright 2019 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# Copyright 2019-today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     "name": "Project Cost Report",
-    "version": "16.0.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://bit.ly/numigi-com",
