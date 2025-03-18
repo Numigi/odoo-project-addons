bc4c5073a43cfa38613e715575cb1c15027ae0a3
Abdellatif BENZBIRIA <a.benzbiria@gmail.com>
2025-03-18 23:04:15 +0000

    Update project_milestone.py

--- project_milestone_timeline_dynamic/models/project_milestone.py
+++ project_milestone_timeline_dynamic/models/project_milestone.py
@@ -10,44 +10,51 @@ class ProjectMilestone(models.Model):
 
     @api.constrains('start_date')
     def _check_start_date_and_dependencies_end_date(self):
-        for milestone in self:
-            sorted_child_ids = milestone.child_ids.sorted(
-                key='target_date', reverse=True)
-            child_last_end_dates = milestone.child_ids.mapped('target_date')
-
-            if child_last_end_dates and milestone.start_date and \
-                    milestone.start_date < max(child_last_end_dates):
-                raise ValidationError(
-                    _('The milestone %s - %s is dependent on this '
-                      'milestone and has a %s end date.'
-                      ) % (sorted_child_ids[0].name,
-                           sorted_child_ids[0].project_id.name,
-                           fields.Date.to_string(max(child_last_end_dates)))
-                )
-
-    def _check_child_milestones_target_date(self):
-        child_last_end_dates = self.child_ids.mapped('target_date')
+        sorted_child_ids = self.child_ids.sorted(
+            key='target_date', reverse=True)
+        child_last_end_dates = self.child_ids.sorted(
+            key='target_date', reverse=True).mapped('target_date')
         if child_last_end_dates and self.start_date and \
-                self.start_date <= max(child_last_end_dates):
-            milestone_duration = self.target_date - self.start_date
-            self.start_date = max(child_last_end_dates) + timedelta(days=1)
-            self.target_date = self.start_date + milestone_duration
+                self.start_date < child_last_end_dates[0]:
+            raise ValidationError(
+                _('The milestone %s - %s is dependent on this '
+                  'milestone and has a %s end date.'
+                  % (sorted_child_ids[0].name,
+                     sorted_child_ids[0].project_id.name,
+                     fields.Date.to_string(child_last_end_dates[0]))
+                  )
+            )
 
-    def _get_parent_milestone(self):
-        self.env.cr.execute(
-            'SELECT milestone_id FROM rel_project_milestone_dependencies '
-            'WHERE child_id = %s', (self.id,))
-        milestone_id = self.env.cr.fetchone()
+    def _check_child_milestones_target_date(self, milestone):
+        child_last_end_dates = milestone.child_ids.sorted(
+            key='target_date', reverse=True).mapped('target_date')
+        if child_last_end_dates and \
+                (not milestone.start_date or milestone.start_date <= \
+                    child_last_end_dates[0]):
+                milestone_duration = \
+                    milestone.target_date - milestone.start_date
+                milestone.start_date = \
+                    child_last_end_dates[0] + timedelta(days=1)
+                milestone.target_date = \
+                    milestone.start_date + milestone_duration
+
+    def _get_parent_milestone(self, milestone):
+        self._cr.execute(
+            'SELECT milestone_id '
+            'FROM "rel_project_milestone_dependencies"'
+            ' WHERE child_id=%s' % milestone.id)
+        milestone_id = self._cr.fetchone()
         return self.env['project.milestone'].browse(milestone_id[0]) \
             if milestone_id else None
 
+    @api.multi
     def write(self, vals):
         for milestone in self:
+            res = super(ProjectMilestone, milestone).write(vals)
             if 'child_ids' in vals:
-                milestone._check_child_milestones_target_date()
+                milestone._check_child_milestones_target_date(milestone)
             if 'target_date' in vals:
-                parent_milestone = milestone._get_parent_milestone()
+                parent_milestone = self._get_parent_milestone(milestone)
                 if parent_milestone:
-                    parent_milestone._check_child_milestones_target_date()
-
-        return super(ProjectMilestone, self).write(vals)
+                    milestone._check_child_milestones_target_date(parent_milestone)
+            return res
