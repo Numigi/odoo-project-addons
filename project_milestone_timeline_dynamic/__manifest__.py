85516e712c674af85297f817627a90e9b92d0fd0
Abdellatif BENZBIRIA <a.benzbiria@gmail.com>
2025-03-20 18:17:23 +0000

    TA#73243 [12.0][FIX] project_milestone_timeline_dynamic : check before write (#525)
    * Update project_milestone.py
    
    * Update project_milestone.py
    
    * Update project_milestone.py
    
    * TA#73243 [12.0][CHG] project_milestone_timeline_dynamic : version and quotes
    
    * [IMP] proejct_milestone_timeline_dynamic
    
    * [IMP] proejct_milestone_timeline_dynamic
    
    * [IMP] proejct_milestone_timeline_dynamic
    
    * [IMP] proejct_milestone_timeline_dynamic
    
    * TA#73243 [12.0][FIX] project_milestone_timeline_dynamic : parent update from childs
    
    Update test_project_milestone_timeline_dynamic.py
    
    * Update .pre-commit-config.yaml
    
    * Test on pre-commit
    
    * Update .pre-commit-config.yaml
    
    * Using dummy precommit
    
    Update pre-commit.yml
    
    Force precommit workflows to not failed
    
    ---------
    
    Co-authored-by: Lanto R. <124650562+lanto-razafindrabe@users.noreply.github.com>

--- project_milestone_timeline_dynamic/__manifest__.py
+++ project_milestone_timeline_dynamic/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Project Milestone Timeline Dynamic",
-    "version": "1.0.0",
+    "version": "12.0.2.0.0",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://bit.ly/numigi-com",
--- project_milestone_timeline_dynamic/models/project_milestone.py
+++ project_milestone_timeline_dynamic/models/project_milestone.py
@@ -1,60 +1,89 @@
 # © 2022 - Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-from odoo import models, api, fields, _
+
 from datetime import timedelta
+
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
 
 class ProjectMilestone(models.Model):
     _inherit = "project.milestone"
 
-    @api.constrains('start_date')
+    @api.constrains("start_date")
     def _check_start_date_and_dependencies_end_date(self):
-        sorted_child_ids = self.child_ids.sorted(
-            key='target_date', reverse=True)
-        child_last_end_dates = self.child_ids.sorted(
-            key='target_date', reverse=True).mapped('target_date')
-        if child_last_end_dates and self.start_date and \
-                self.start_date < child_last_end_dates[0]:
-            raise ValidationError(
-                _('The milestone %s - %s is dependent on this '
-                  'milestone and has a %s end date.'
-                  % (sorted_child_ids[0].name,
-                     sorted_child_ids[0].project_id.name,
-                     fields.Date.to_string(child_last_end_dates[0]))
-                  )
+        for milestone in self:
+            sorted_child_ids = milestone.child_ids.sorted(
+                key="target_date", reverse=True
             )
+            child_last_end_dates = milestone.child_ids.mapped("target_date")
 
-    def _check_child_milestones_target_date(self, milestone):
-        child_last_end_dates = milestone.child_ids.sorted(
-            key='target_date', reverse=True).mapped('target_date')
-        if child_last_end_dates and \
-                (not milestone.start_date or milestone.start_date <= \
-                    child_last_end_dates[0]):
-                milestone_duration = \
-                    milestone.target_date - milestone.start_date
-                milestone.start_date = \
-                    child_last_end_dates[0] + timedelta(days=1)
-                milestone.target_date = \
-                    milestone.start_date + milestone_duration
+            if (
+                child_last_end_dates
+                and milestone.start_date
+                and milestone.start_date < max(child_last_end_dates)
+            ):
+                raise ValidationError(
+                    _(
+                        "The milestone %s - %s is dependent on this "
+                        "milestone and has a %s end date."
+                    )
+                    % (
+                        sorted_child_ids[0].name,
+                        sorted_child_ids[0].project_id.name,
+                        fields.Date.to_string(max(child_last_end_dates)),
+                    )
+                )
 
-    def _get_parent_milestone(self, milestone):
-        self._cr.execute(
-            'SELECT milestone_id '
-            'FROM "rel_project_milestone_dependencies"'
-            ' WHERE child_id=%s' % milestone.id)
-        milestone_id = self._cr.fetchone()
-        return self.env['project.milestone'].browse(milestone_id[0]) \
-            if milestone_id else None
+    @api.onchange("child_ids")
+    def _onchange_child_ids(self):
+        child_last_end_dates = self.child_ids.sorted(
+            key="target_date", reverse=True
+        ).mapped("target_date")
 
-    @api.multi
+        self._assign_new_timeline(
+            self.start_date, self.target_date, child_last_end_dates
+        )
+
     def write(self, vals):
         for milestone in self:
-            res = super(ProjectMilestone, milestone).write(vals)
-            if 'child_ids' in vals:
-                self._check_child_milestones_target_date(milestone)
-            if 'target_date' in vals:
-                parent_milestone = self._get_parent_milestone(milestone)
+            if "target_date" in vals:
+                parent_milestone = milestone._get_parent_milestone()
                 if parent_milestone:
-                    self._check_child_milestones_target_date(parent_milestone)
-            return res
+                    parent_milestone.update_parent_timeline(milestone, vals)
+        return super(ProjectMilestone, self).write(vals)
+
+    def _assign_new_timeline(self, start_date, target_date, child_last_end_dates):
+        if child_last_end_dates and (
+            not start_date or start_date <= child_last_end_dates[0]
+        ):
+            milestone_duration = target_date - start_date
+            self.start_date = child_last_end_dates[0] + timedelta(days=1)
+            self.target_date = self.start_date + milestone_duration
+
+    def _get_parent_milestone(self):
+        self.env.cr.execute(
+            "SELECT milestone_id FROM rel_project_milestone_dependencies "
+            "WHERE child_id = %s",
+            (self.id,),
+        )
+        milestone_id = self.env.cr.fetchone()
+        return (
+            self.env["project.milestone"].browse(milestone_id[0])
+            if milestone_id
+            else None
+        )
+
+    def update_parent_timeline(self, child_milestone, updated_val):
+        child_ids = self.child_ids - child_milestone
+        child_last_end_dates = child_ids.sorted(key="target_date", reverse=True).mapped(
+            "target_date"
+        )
+        child_target_date = fields.Date.from_string(updated_val["target_date"])
+
+        if child_target_date not in child_last_end_dates:
+            child_last_end_dates.append(child_target_date)
+            child_last_end_dates.sort(reverse=True)
+            self._assign_new_timeline(
+                self.start_date, self.target_date, child_last_end_dates
+            )
