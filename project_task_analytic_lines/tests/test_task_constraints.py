b639fb3acc8d7c7bd3cfa08848ac701fdaded40a
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2025-05-05 15:15:06 +0100

    TA#77191 [FIX] project_task_analytic_lines : TU

--- project_task_analytic_lines/tests/test_task_constraints.py
+++ project_task_analytic_lines/tests/test_task_constraints.py
@@ -70,16 +70,16 @@ class TestAnalyticLineConstraints(AccountCase):
             line.project_id = self.project_2
 
     def test_after_changing_task__if_task_not_matching_analytic_account__raise_error(
-         self,
-    ):
-         line = self.env["account.analytic.line"].create(
-             {
-                 "name": "/",
-                 "project_id": self.project.id,
-                 "task_id": self.task.id,
-                 "user_id": self.account_user.id,
-             }
-         )
-         self.task_2.project_id = self.project_2
-         with pytest.raises(ValidationError):
-             line.origin_task_id = self.task_2
+        self,
+     ):
+        line = self.env["account.analytic.line"].create(
+            {
+                "name": "/",
+                "project_id": self.project.id,
+                "task_id": self.task.id,
+                "user_id": self.account_user.id,
+            }
+        )
+        self.task_2.project_id = self.project_2
+        with pytest.raises(ValidationError):
+            line.origin_task_id = self.task_2
