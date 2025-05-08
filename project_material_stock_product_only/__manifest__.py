a10b0413521f8c1e5dd13b8c44b6f59dde7de49c
Rivo Lalaina <rivo2302@gmail.com>
2025-05-08 14:04:35 +0300

    Update __manifest__.py

--- project_material_stock_product_only/__manifest__.py
+++ project_material_stock_product_only/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Project Material Stock Product Only",
-    "version": "14.0.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://bit.ly/numigi-com",
