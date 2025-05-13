d9de81f54c0dd2f0a22221bf747ec3f5ac4a2ef3
Rivo Lalaina <rivo2302@gmail.com>
2025-05-14 01:57:41 +0300

    Ta#72167 [16.0][MIG] project_milestone_report (#558)
    * [16.0][MIG] project_milestone reprot
    
    * [16.0][MIG] project_milestone_time_report
    
    * [16.0][MIG] project_milestone_time_report

---
+++ project_milestone_time_report/__manifest__.py
@@ -0,0 +1,31 @@
+# Copyright 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Project Report Milestone Time",
+    "version": "16.0.1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "LGPL-3",
+    "category": "Project",
+    "summary": "Add a dynamic milestone time report on projects",
+    "depends": [
+        "project_parent_enhanced",
+        "project_lump_sum",
+        "project_milestone_time_kpi",
+    ],
+    "data": [
+        "report/report.xml",
+        "views/project_project_views.xml",
+        "views/project_milestone_views.xml",
+    ],
+    "assets": {
+        "web.assets_backend": [
+            "project_milestone_time_report/static/src/js/report_action.js",
+            "project_milestone_time_report/static/src/scss/report.scss",
+            "project_milestone_time_report/static/src/xml/**/*",
+        ]
+    },
+    "installable": True,
+}
