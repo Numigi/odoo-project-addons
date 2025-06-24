2cad3b31a11f6ed6766b0d34e781d1feb032a14c
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2025-06-24 17:08:22 +0100

    TA#78188 [FIX] project_wip_closing_batch compute balance  basing on date_cutoff

--- project_wip_batch_closing/__manifest__.py
+++ project_wip_batch_closing/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Project WIP Batch Closing",
-    "version": "14.0.1.1.0",
+    "version": "14.0.1.2.0",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://bit.ly/numigi-com",
