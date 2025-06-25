dd885121664ac6e4ad14c7c22cf5b11b694a0b32
Abdellatif BENZBIRIA <a.benzbiria@gmail.com>
2025-06-25 19:12:54 +0100

    TA#78188  [IMP] project_wip_batch_closing : add archived projects (#569)
    * [16.0][MIG] project_cost_report
    
    * [16.0] [MIG] Project cost report
    
    * [14.0][MIG] project_cost_report
    
    * Update Dockerfile
    
    * resolve conflict
    
    * resolve conflict
    
    * TA#78188 [FIX] project_wip_closing_batch compute balance  basing on date_cutoff
    
    * TA#78188 [FIX] project_wip_closing_batch compute balance  basing on date_cutoff
    
    * TA#78188 [FIX] project_wip_closing_batch compute balance  basing on date_cutoff
    
    * TA#78188 [FIX] project_wip_closing_batch compute balance  basing on date_cutoff
    
    * TA#78188 [FIX] project_wip_closing_batch compute balance  basing on date_cutoff
    
    * TA#78188 [IMP] project_wip_batch_closing : add archived projects
    
    ---------
    
    Co-authored-by: Rivo Lalaina <rivo2302@gmail.com>

--- project_wip_batch_closing/wizard/project_wip_batch_closing.py
+++ project_wip_batch_closing/wizard/project_wip_batch_closing.py
@@ -35,7 +35,7 @@ class ProjectWipBatchClosing(models.TransientModel):
             ("type_id.cgs_account_id", "!=", False),
         ]
 
-        project_ids = self.env["project.project"].search(domain)
+        project_ids = self.env["project.project"].with_context(active_test=False).search(domain)
         record_values = []
         for project in project_ids:
             costs_to_transfer = sum(
