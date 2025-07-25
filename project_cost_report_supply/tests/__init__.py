cb64b2d7e2461ccaea660cd792973b1cdde169ef
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2025-07-25 15:56:53 +0100

    [ADD] TU project_cost_report_supply

---
+++ project_cost_report_supply/tests/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2019-today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import test_analytic_line
+from . import test_report
+
