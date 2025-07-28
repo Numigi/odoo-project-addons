7f2aee2119a9287da682ac4b631ac217aa08fbd3
Abdellatif Benzbiria <a.benzbiria@gmail.com>
2025-07-28 15:30:26 +0100

    [IMP] project_cost_report_supply TU

--- project_cost_report_supply/models/account_analytic_line.py
+++ project_cost_report_supply/models/account_analytic_line.py
@@ -5,16 +5,13 @@ from odoo import api, fields, models
 
 
 class AnalyticLine(models.Model):
-
     _inherit = "account.analytic.line"
-
 
     project_cost_section = fields.Selection(
         selection_add=[('supply', 'Shop Supply')],
         compute="_compute_project_cost_section",
         store=True
     )
-
 
     @api.depends(
         "is_shop_supply",
@@ -30,14 +27,8 @@ class AnalyticLine(models.Model):
             return "supply"
         return super(AnalyticLine, self)._get_project_cost_section()
 
-
-
     def _get_project_cost_category(self):
-        res=  super(AnalyticLine, self)._get_project_cost_category()
+        res = super(AnalyticLine, self)._get_project_cost_category()
         if self.project_cost_section == "supply":
             return self.env.ref("project_cost_report_supply.cost_category_supply", False)
         return res
-
-
-
-
