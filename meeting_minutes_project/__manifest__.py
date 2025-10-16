8331ea778c93105e14975568179919f02796c413
Abdellatif Benzbiria <odoo-base@numigi.com>
2025-10-16 16:27:47 +0100

    TA#80300 [IMP] pre-commit and homework_ids

--- meeting_minutes_project/__manifest__.py
+++ meeting_minutes_project/__manifest__.py
@@ -1,4 +1,4 @@
-# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# Copyright 2025 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
