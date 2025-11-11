7df2b6e8f2318900dea6dae82e8a396a541b7262
Abdellatif BENZBIRIA <a.benzbiria@gmail.com>
2025-11-11 15:52:49 +0100

    [I18N] project_task_analytic_lines : trad task (#586)
    Co-authored-by: Abdellatif Benzbiria <odoo-base@numigi.com>

--- project_task_analytic_lines/__manifest__.py
+++ project_task_analytic_lines/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Project Task Analytic Lines',
-    'version': '14.0.1.1.0',
+    'version': '14.0.1.1.1',
     'author': 'Numigi',
     'maintainer': 'Numigi',
     'website': 'https://numigi.com/r/home',
