debf1c150f89525ea2f0760059259c1363b5467c
Abdellatif BENZBIRIA <a.benzbiria@gmail.com>
2026-01-19 12:49:36 +0100

    TA#82478 [IMP] project_estimation_material : auto assign disabl (#588)
    * Update project_milestone.py
    
    * Update project_milestone.py
    
    * Update project_milestone.py
    
    * TA#73243 [12.0][CHG] project_milestone_timeline_dynamic : version and quotes
    
    * TA#82478 [IMP] project_estimation_material : auto assign disable
    
    ---------
    
    Co-authored-by: Lanto R. <124650562+lanto-razafindrabe@users.noreply.github.com>
    Co-authored-by: Abdellatif Benzbiria <odoo-base@numigi.com>

--- project_estimation_material/wizard/project_estimation_exit.py
+++ project_estimation_material/wizard/project_estimation_exit.py
@@ -2,6 +2,9 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo import api, models, fields
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class ProjectEstimationExit(models.TransientModel):
@@ -19,8 +22,18 @@ class ProjectEstimationExit(models.TransientModel):
         )
 
     def validate(self):
-        super().validate()
-        self._trigger_procurements()
+        _logger.info("Project Estimation Exit: Start validation")
+        ctx = dict(self.env.context)
+        auto_assign_config = self.env["ir.config_parameter"].sudo().get_param(
+            "stock_auto_assign_disabled.config", "off"
+        )
+        if auto_assign_config == 'all':
+            ctx.update({
+                'stock_auto_assign_disable': True,  # Force the auto assign disable
+            })
+        res = super(ProjectEstimationExit, self.with_context(ctx)).validate()
+        self.with_context(ctx)._trigger_procurements()
+        return res
 
     def _trigger_procurements(self):
         for line in self.project_id.material_line_ids:
