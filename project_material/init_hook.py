2cce0c1e3beafd22f164501551fcaa689134983c
Abdellatif Benzbiria <odoo-base@numigi.com>
2026-03-06 17:59:55 +0000

    TA#83287 [IMP] project_material : compute consu location by company

--- project_material/init_hook.py
+++ project_material/init_hook.py
@@ -12,6 +12,8 @@ def post_init_hook(cr, registry):
 def _setup_warehouses_consumption_routes(env):
     warehouses = env["stock.warehouse"].search([])
     for warehouse in warehouses:
+        warehouse._set_project_consu_location(warehouse.company_id.id)
+        warehouse._compute_consu_location_id()
         warehouse._create_or_update_consumption_picking_types()
         warehouse._create_or_update_consumption_route()
         warehouse._create_consumption_mto_pull()
--- project_wip_material/models/__init__.py
+++ project_wip_material/models/__init__.py
@@ -2,6 +2,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from . import (
+    account_move_line,
     project_task_material,
     stock_move,
 )
