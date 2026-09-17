8542b17a999fc70de934657127b950ca2d22f3a4
Abdellatif BENZBIRIA <a.benzbiria@gmail.com>
2026-09-17 13:13:25 +0100

    TA#85715 [14.0][IMP] project_worksheet : Customize Emails and Subscribers (#608)
    * [14.0][IMP] project_worksheet : Customize Emails and Subscribers
    
    * [14.0][IMP] project_worksheet : Customize Emails and Subscribers
    
    ---------
    
    Co-authored-by: Abdellatif Benzbiria <odoo-base@numigi.com>

--- project_worksheet/models/project_worksheet.py
+++ project_worksheet/models/project_worksheet.py
@@ -204,8 +204,12 @@ class ProjectWorksheet(models.Model):
             self._generate_timesheets()
 
     def _send_approval_email(self):
-        template = self.env.ref("project_worksheet.email_template_worksheet_approval")
-        template.send_mail(self.id, force_send=True)
+        template_id = self.env.ref("project_worksheet.email_template_worksheet_approval").id
+        self.message_post_with_template(
+            template_id,
+            composition_mode="comment",
+            message_type="comment",
+        )
 
     def _get_remind_client_action(self):
         template = self.env.ref("project_worksheet.email_template_worksheet_approval")
@@ -239,9 +243,31 @@ class ProjectWorksheet(models.Model):
         self._send_confirmation_email()
 
     def _send_confirmation_email(self):
-        template = self.env.ref("project_worksheet.email_template_worksheet_confirmed")
+        template_id = self._get_confirmation_template_id()
         for worksheet in self:
-            template.send_mail(worksheet.id, force_send=True)
+            worksheet._post_confirmation_message(template_id)
+
+    def _get_confirmation_template_id(self):
+        return self.env.ref("project_worksheet.email_template_worksheet_confirmed").id
+
+    def _post_confirmation_message(self, template_id):
+        # Post the message using the appropriate user context
+        record = self._get_mail_sender_record()
+        record.message_post_with_template(
+            template_id,
+            composition_mode="comment",
+            message_type="comment",
+        )
+
+    def _get_mail_sender_record(self):
+        # Use root user (OdooBot) if the client is the one confirming the worksheet
+        if self.approval_source == "client":
+            return self._get_root_user_record()
+        return self
+
+    def _get_root_user_record(self):
+        root_user = self.env.ref("base.user_root")
+        return self.with_user(root_user)
 
     def _generate_timesheets(self):
         timesheet_model = self.env["account.analytic.line"]
