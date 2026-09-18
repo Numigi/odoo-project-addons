bf979caeafb47f889ea9a8664b48919e90381b02
Abdellatif Benzbiria <odoo-base@numigi.com>
2026-09-18 18:03:06 +0100

    [14.0][IMP] project_worksheet : Customize Attached Emails

--- project_worksheet/models/project_worksheet_line.py
+++ project_worksheet/models/project_worksheet_line.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, fields, models
-from odoo.exceptions import UserError, ValidationError
+from odoo.exceptions import ValidationError
 from odoo.tools.translate import _
 
 
